deebbca1b8f060f31d85e6e1b3f6ac0f3eac144a
Jim Counts <jamesrcounts@outlook.com>
2023-10-20 03:07:53 +0000

    Formatting

--- deploy/scripts/BuildPush.ps1
+++ deploy/scripts/BuildPush.ps1
@@ -1,26 +1,26 @@
 #! /usr/bin/pwsh
 
 Param(
-    [parameter(Mandatory=$true)][string]$resourceGroup,
-    [parameter(Mandatory=$true)][string]$acrName,
-    [parameter(Mandatory=$false)][bool]$dockerBuild=$true,
-    [parameter(Mandatory=$false)][bool]$dockerPush=$true,
-    [parameter(Mandatory=$false)][string]$dockerTag="latest",
-    [parameter(Mandatory=$false)][bool]$isWindowsMachine=$false
+    [parameter(Mandatory = $true)][string]$resourceGroup,
+    [parameter(Mandatory = $true)][string]$acrName,
+    [parameter(Mandatory = $false)][bool]$dockerBuild = $true,
+    [parameter(Mandatory = $false)][bool]$dockerPush = $true,
+    [parameter(Mandatory = $false)][string]$dockerTag = "latest",
+    [parameter(Mandatory = $false)][bool]$isWindowsMachine = $false
 )
 
 Push-Location $($MyInvocation.InvocationName | Split-Path)
-$sourceFolder=$(./Join-Path-Recursively.ps1 -pathParts ..,scripts)
+$sourceFolder = $(./Join-Path-Recursively.ps1 -pathParts .., scripts)
 Write-Host "---------------------------------------------------" -ForegroundColor Yellow
 
 Write-Host "---------------------------------------------------" -ForegroundColor Yellow
 Write-Host "Getting info from ACR $resourceGroup/$acrName" -ForegroundColor Yellow
 Write-Host "---------------------------------------------------" -ForegroundColor Yellow
-$acrLoginServer=$(az acr show -g $resourceGroup -n $acrName -o json | ConvertFrom-Json).loginServer
-$acrCredentials=$(az acr credential show -g $resourceGroup -n $acrName -o json | ConvertFrom-Json)
-$acrPwd=$acrCredentials.passwords[0].value
-$acrUser=$acrCredentials.username
-$dockerComposeFile="../docker/docker-compose.yml"
+$acrLoginServer = $(az acr show -g $resourceGroup -n $acrName -o json | ConvertFrom-Json).loginServer
+$acrCredentials = $(az acr credential show -g $resourceGroup -n $acrName -o json | ConvertFrom-Json)
+$acrPwd = $acrCredentials.passwords[0].value
+$acrUser = $acrCredentials.username
+$dockerComposeFile = "../docker/docker-compose.yml"
 
 
 if ($dockerBuild) {
@@ -30,8 +30,8 @@ if ($dockerBuild) {
     Write-Host "---------------------------------------------------" -ForegroundColor Yellow
 
     Push-Location $sourceFolder
-    $env:TAG=$dockerTag
-    $env:REGISTRY=$acrLoginServer 
+    $env:TAG = $dockerTag
+    $env:REGISTRY = $acrLoginServer 
     docker-compose -f $dockerComposeFile build
     Pop-Location
 }
@@ -43,8 +43,8 @@ if ($dockerPush) {
 
     Push-Location $sourceFolder
     docker login -p $acrPwd -u $acrUser $acrLoginServer
-    $env:TAG=$dockerTag
-    $env:REGISTRY=$acrLoginServer 
+    $env:TAG = $dockerTag
+    $env:REGISTRY = $acrLoginServer 
     docker-compose -f $dockerComposeFile push
     Pop-Location
 }
