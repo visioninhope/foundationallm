1e17b6642b83c7bc95a4c7f19cc0638b7f2a6d7c
Sensational Code <sensational.code@gmail.com>
2023-10-23 03:34:09 -0700

    Formatting

--- src/UI/UserPortal/middleware/auth.global.ts
+++ src/UI/UserPortal/middleware/auth.global.ts
@@ -1,7 +1,7 @@
 import { msalInstance } from '@/js/auth';
 export default defineNuxtRouteMiddleware(async (to) => {
-    const accounts = await msalInstance.getAllAccounts();    
-    if (accounts.length === 0 && to.path !== '/login') {
-        return navigateTo('/login');
-    }
+	const accounts = await msalInstance.getAllAccounts();    
+	if (accounts.length === 0 && to.path !== '/login') {
+		return navigateTo('/login');
+	}
 });
--- src/UI/UserPortal/server/api.ts
+++ src/UI/UserPortal/server/api.ts
@@ -17,7 +17,7 @@ export default {
 		return this.bearerToken;
 	},
 
-  async fetch(url: string, opts: any = {}) {
+	async fetch(url: string, opts: any = {}) {
 		const options = opts;
 		options.headers = opts.headers || {};
 
@@ -28,63 +28,63 @@ export default {
 		const bearerToken = await this.getBearerToken();
 		options.headers['Authorization'] = `Bearer ${bearerToken}`;
 
-    return await $fetch(url, options);
-  },
+		return await $fetch(url, options);
+	},
 
-  async getSessions() {
-    return await this.fetch(`${API_URL}/sessions`) as Array<Session>;
-  },
+	async getSessions() {
+		return await this.fetch(`${API_URL}/sessions`) as Array<Session>;
+	},
 
-  async addSession() {
-    return await this.fetch(`${API_URL}/sessions`, { method: 'POST' }) as Session;
-  },
+	async addSession() {
+		return await this.fetch(`${API_URL}/sessions`, { method: 'POST' }) as Session;
+	},
 
-  async renameSession(sessionId: string, newChatSessionName: string) {
-    return await this.fetch(`${API_URL}/sessions/${sessionId}/rename`, {
-      method: 'POST',
-      params: {
-        newChatSessionName
-      }
-    }) as Session;
-  },
+	async renameSession(sessionId: string, newChatSessionName: string) {
+		return await this.fetch(`${API_URL}/sessions/${sessionId}/rename`, {
+			method: 'POST',
+			params: {
+				newChatSessionName
+			}
+		}) as Session;
+	},
 
-  async summarizeSessionName(sessionId: string, text: string) {
-    return await this.fetch(`${API_URL}/sessions/${sessionId}/summarize-name`, {
-      method: 'POST',
-      body: JSON.stringify(text),
-    }) as { text: string };
-  },
+	async summarizeSessionName(sessionId: string, text: string) {
+		return await this.fetch(`${API_URL}/sessions/${sessionId}/summarize-name`, {
+			method: 'POST',
+			body: JSON.stringify(text),
+		}) as { text: string };
+	},
 
-  async deleteSession(sessionId: string) {
-    return await this.fetch(`${API_URL}/sessions/${sessionId}`, { method: 'DELETE' }) as Session;
-  },
+	async deleteSession(sessionId: string) {
+		return await this.fetch(`${API_URL}/sessions/${sessionId}`, { method: 'DELETE' }) as Session;
+	},
 
-  async getMessages(sessionId: string) {
-    return await this.fetch(`${API_URL}/sessions/${sessionId}/messages`) as Array<Message>;
-  },
+	async getMessages(sessionId: string) {
+		return await this.fetch(`${API_URL}/sessions/${sessionId}/messages`) as Array<Message>;
+	},
 
-  async getPrompt(sessionId: string, promptId: string) {
-    return await this.fetch(`${API_URL}/sessions/${sessionId}/completionprompts/${promptId}`) as CompletionPrompt;
-  },
+	async getPrompt(sessionId: string, promptId: string) {
+		return await this.fetch(`${API_URL}/sessions/${sessionId}/completionprompts/${promptId}`) as CompletionPrompt;
+	},
 
-  async rateMessage(message: Message, rating: Message['rating']) {
-    const params: {
-      rating?: Message['rating']
-    } = {};
-    if (rating !== null) params.rating = rating;
+	async rateMessage(message: Message, rating: Message['rating']) {
+		const params: {
+			rating?: Message['rating']
+		} = {};
+		if (rating !== null) params.rating = rating;
 
-    return await this.fetch(
-      `${API_URL}/sessions/${message.sessionId}/message/${message.id}/rate`, {
-        method: 'POST',
-        params
-      }
-    ) as Message;
-  },
+		return await this.fetch(
+			`${API_URL}/sessions/${message.sessionId}/message/${message.id}/rate`, {
+				method: 'POST',
+				params
+			}
+		) as Message;
+	},
 
-  async sendMessage(sessionId: string, text: string) {
-    return (await this.fetch(`${API_URL}/sessions/${sessionId}/completion`, {
-      method: 'POST',
-      body: JSON.stringify(text),
-    })) as string;
-  },
+	async sendMessage(sessionId: string, text: string) {
+		return (await this.fetch(`${API_URL}/sessions/${sessionId}/completion`, {
+			method: 'POST',
+			body: JSON.stringify(text),
+		})) as string;
+	},
 };
