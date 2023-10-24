2d61696197ea72ab4277dceadef9c333990debb0
joelhulen <joelhulen@gmail.com>
2023-10-24 19:44:27 -0400

    Add XML documentation to the Orchestration controller

--- src/dotnet/CoreAPI/Controllers/OrchestrationController.cs
+++ src/dotnet/CoreAPI/Controllers/OrchestrationController.cs
@@ -12,6 +12,9 @@ using Newtonsoft.Json;
 
 namespace FoundationaLLM.Core.API.Controllers
 {
+    /// <summary>
+    /// Methods for orchestration services exposed by the Gatekeeper API service.
+    /// </summary>
     [Authorize]
     [Authorize(Policy = "RequiredScope")]
     [ApiVersion(1.0)]
@@ -22,6 +25,12 @@ namespace FoundationaLLM.Core.API.Controllers
         private readonly IGatekeeperAPIService _gatekeeperAPIService;
         private readonly ILogger<OrchestrationController> _logger;
 
+        /// <summary>
+        /// Constructor for the Orchestration Controller.
+        /// </summary>
+        /// <param name="gatekeeperAPIService">Provides methods for calling the Gatekeeper API.</param>
+        /// <param name="logger">The logging interface used to log under the
+        /// <see cref="OrchestrationController"/> type name.</param>
         public OrchestrationController(IGatekeeperAPIService gatekeeperAPIService,
             ILogger<OrchestrationController> logger)
         {
@@ -29,6 +38,11 @@ namespace FoundationaLLM.Core.API.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Requests a completion from the downstream APIs via the Gatekeeper API.
+        /// </summary>
+        /// <param name="completionRequest">The completion request containing the user
+        /// prompt and message history.</param>
         [AllowAnonymous]
         [HttpPost("completion", Name = "GetCompletion")]
         public async Task<IActionResult> GetCompletion(CompletionRequest completionRequest)
@@ -38,6 +52,11 @@ namespace FoundationaLLM.Core.API.Controllers
             return Ok(completionResponse);
         }
 
+        /// <summary>
+        /// Requests a summary from the downstream APIs via the Gatekeeper API.
+        /// </summary>
+        /// <param name="summaryRequest">The summary request containing the user
+        /// prompt.</param>
         [AllowAnonymous]
         [HttpPost("summary", Name = "GetSummary")]
         public async Task<IActionResult> GetSummary(SummaryRequest summaryRequest)
