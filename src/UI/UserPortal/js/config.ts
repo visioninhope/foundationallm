343e161bd757b596cc282c31b6c94767ce89ef90
Sensational Code <sensational.code@gmail.com>
2023-10-25 19:49:10 -0700

    Formatting

--- src/UI/UserPortal/js/config.ts
+++ src/UI/UserPortal/js/config.ts
@@ -1,11 +1,11 @@
-import { AppConfigurationClient } from "@azure/app-configuration";
+import { AppConfigurationClient } from '@azure/app-configuration';
 
 declare const APP_CONFIG_ENDPOINT: string;
 
 const appConfigEndpoint = APP_CONFIG_ENDPOINT;
 
 if (!appConfigEndpoint) {
-	throw new Error("APP_CONFIG_ENDPOINT environment variable is not defined");
+	throw new Error('APP_CONFIG_ENDPOINT environment variable is not defined');
 }
 
 const appConfigClient = new AppConfigurationClient(appConfigEndpoint);
