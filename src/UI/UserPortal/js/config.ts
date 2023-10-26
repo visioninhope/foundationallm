edb6231dc329265c3872aac0df449d32383e7ac4
Sensational Code <sensational.code@gmail.com>
2023-10-25 20:10:18 -0700

    Update azure config client creation

--- src/UI/UserPortal/js/config.ts
+++ src/UI/UserPortal/js/config.ts
@@ -1,16 +1,21 @@
 import { AppConfigurationClient } from '@azure/app-configuration';
-
 declare const APP_CONFIG_ENDPOINT: string;
 
-const appConfigEndpoint = APP_CONFIG_ENDPOINT;
+let appConfigClient = new AppConfigurationClient(APP_CONFIG_ENDPOINT);
 
-if (!appConfigEndpoint) {
-	throw new Error('APP_CONFIG_ENDPOINT environment variable is not defined');
+function getConfigClient() {
+	if (!APP_CONFIG_ENDPOINT) {
+		// throw new Error('APP_CONFIG_ENDPOINT environment variable is not defined');
+	}
+
+	if (!appConfigClient) {
+		appConfigClient = new AppConfigurationClient(APP_CONFIG_ENDPOINT);
+	}
+
+	return appConfigClient;
 }
 
-const appConfigClient = new AppConfigurationClient(appConfigEndpoint);
-
 export default async function getAppConfigSetting(key: string) {
-	const setting = await appConfigClient.getConfigurationSetting({ key });
+	const setting = await getConfigClient().getConfigurationSetting({ key });
 	return setting.value ? setting.value : null;
 }
