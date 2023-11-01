67376d7006a8fe90befb2df25aa07bb8febbef87
kylebunting <kyle.bunting@gmail.com>
2023-11-01 11:56:29 -0600

    Deleting unused models from PythonSDK

--- src/python/PythonSDK/foundationallm/models/session.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List, Optional
-from foundationallm.models import Message
-
-class Session(BaseModel):    
-    user_prompt: str
-    message_history: Optional[List[Message]] = None
-    user_context: str
