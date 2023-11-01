2e286e049b2843c4f21d364390dbdb6b7a4c0ac5
Sensational Code <sensational.code@gmail.com>
2023-11-01 10:23:14 -0700

    Use dynamic import instead of require

--- src/ui/UserPortal/js/config.ts
+++ src/ui/UserPortal/js/config.ts
@@ -1,15 +1,15 @@
-// Node may try dns resolution with IPv6 first which breaks the azure app
-// configuration service requests, so we need to force it use IPv4 instead.
-if (process.server) {
-	const dns = require('node:dns');
-	dns.setDefaultResultOrder('ipv4first');
-}
-
 import { AppConfigurationClient } from '@azure/app-configuration';
 
 let appConfigClient = null;
 
-function getConfigClient() {
+async function getConfigClient() {
+	// Node may try dns resolution with IPv6 first which breaks the azure app
+	// configuration service requests, so we need to force it use IPv4 instead.
+	if (process.server) {
+		const dns = await import('node:dns');
+		dns.setDefaultResultOrder('ipv4first');
+	}
+
 	if (!appConfigClient) {
 		appConfigClient = new AppConfigurationClient(process.env.APP_CONFIG_ENDPOINT);
 	}
@@ -18,6 +18,7 @@ function getConfigClient() {
 }
 
 export async function getAppConfigSetting(key: string) {
-	const setting = await getConfigClient().getConfigurationSetting({ key });
+	const client = await getConfigClient();
+	const setting = await client.getConfigurationSetting({ key });
 	return setting.value ? setting.value : null;
 }
