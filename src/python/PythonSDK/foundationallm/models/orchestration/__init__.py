2af5cbac99c38183e8a9e9bc0b496b1efd649f52
kylebunting <kyle.bunting@gmail.com>
2023-11-01 12:01:46 -0600

    Merging orchestration base classes into completion classes.

--- src/python/PythonSDK/foundationallm/models/orchestration/__init__.py
+++ src/python/PythonSDK/foundationallm/models/orchestration/__init__.py
@@ -1,6 +1,4 @@
 """FoundationaLLM orchestration models module"""
 from .message_history_item import MessageHistoryItem
-from .orchestration_request import OrchestrationRequest
-from .orchestration_response import OrchestrationResponse
 from .completion_request import CompletionRequest
 from .completion_response import CompletionResponse
--- src/python/PythonSDK/foundationallm/models/orchestration/completion_request.py
+++ src/python/PythonSDK/foundationallm/models/orchestration/completion_request.py
@@ -1,13 +1,16 @@
+from pydantic import BaseModel
 from typing import List, Optional
-from foundationallm.models.orchestration import MessageHistoryItem, OrchestrationRequest
+
+from foundationallm.models.orchestration import MessageHistoryItem
 from foundationallm.models.metadata import Agent
 from foundationallm.models.metadata import DataSource
 from foundationallm.models.metadata import LanguageModel
 
-class CompletionRequest(OrchestrationRequest):
+class CompletionRequest(BaseModel):
     """
     Orchestration completion request.
     """
+    user_prompt: str
     agent: Optional[Agent] = None
     data_source: Optional[DataSource] = None
     language_model: Optional[LanguageModel] = None
