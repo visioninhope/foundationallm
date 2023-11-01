d16c2ca16a9737b768af41a31b58fc36c19f51a7
gabrielgheorghescu <gabriel.gheorghescu@htss.ro>
2023-11-01 17:40:55 +0200

    Increase code coverage & added missing unit tests

---
+++ tests/dotnet/Common.Tests/Models/Context/CallContextTests.cs
@@ -0,0 +1,62 @@
+﻿using FoundationaLLM.Common.Models.Authentication;
+using FoundationaLLM.Common.Models.Context;
+using NSubstitute;
+
+namespace FoundationaLLM.Common.Tests.Models.Context
+{
+    public class CallContextTests
+    {
+        [Fact]
+        public void TestAgentHint()
+        {
+            // Arrange
+            var callContext = new CallContext();
+            var agentHint = "TestAgentHint";
+
+            // Act
+            callContext.AgentHint = agentHint;
+
+            // Assert
+            Assert.Equal(agentHint, callContext.AgentHint);
+        }
+
+        [Fact]
+        public void TestCurrentUserIdentityWithNSubstitute()
+        {
+            // Arrange
+            var callContext = new CallContext();
+            var userIdentity = Substitute.For<UnifiedUserIdentity>();
+            userIdentity.Name = "TestName";
+            userIdentity.Username = "TestUsername";
+            userIdentity.UPN = "TestUPN";
+
+            // Act
+            callContext.CurrentUserIdentity = userIdentity;
+
+            // Assert
+            Assert.Equal("TestName", callContext.CurrentUserIdentity.Name);
+            Assert.Equal("TestUsername", callContext.CurrentUserIdentity.Username);
+            Assert.Equal("TestUPN", callContext.CurrentUserIdentity.UPN);
+        }
+
+        [Fact]
+        public void TestDefaultAgentHint()
+        {
+            // Arrange
+            var callContext = new CallContext();
+
+            // Assert
+            Assert.Null(callContext.AgentHint);
+        }
+
+        [Fact]
+        public void TestDefaultCurrentUserIdentity()
+        {
+            // Arrange
+            var callContext = new CallContext();
+
+            // Assert
+            Assert.Null(callContext.CurrentUserIdentity);
+        }
+    }
+}
---
+++ tests/dotnet/Common.Tests/Settings/CommonJsonSerializerSettingsTests.cs
@@ -0,0 +1,25 @@
+﻿using FoundationaLLM.Common.Settings;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace FoundationaLLM.Common.Tests.Settings
+{
+    public class CommonJsonSerializerSettingsTests
+    {
+        [Fact]
+        public void GetJsonSerializerSettings_ShouldReturnValidJsonSerializerSettings()
+        {
+            // Arrange
+            var expectedSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            // Act
+            var actualSettings = CommonJsonSerializerSettings.GetJsonSerializerSettings();
+
+            // Assert
+            Assert.Equal(expectedSettings.ContractResolver.GetType(), actualSettings.ContractResolver?.GetType());
+        }
+    }
+}
