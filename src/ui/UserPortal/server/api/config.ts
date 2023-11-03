b93480bfbffc25181d7da0a2901359c05b62b694
Sensational Code <sensational.code@gmail.com>
2023-11-02 23:43:49 -0700

    Improve error reporting for azure app config key requests

--- src/ui/UserPortal/server/api/config.ts
+++ src/ui/UserPortal/server/api/config.ts
@@ -5,7 +5,7 @@ dns.setDefaultResultOrder('ipv4first');
 
 import { AppConfigurationClient } from '@azure/app-configuration';
 
-const validKeys = [
+const allowedKeys = [
 	'FoundationaLLM:APIs:CoreAPI:APIUrl',
 	'.appconfig.featureflag/FoundationaLLM-AllowAgentHint',
 	'FoundationaLLM:Branding:AllowAgentSelection',
@@ -30,19 +30,26 @@ export default defineEventHandler(async (event) => {
 	const query = getQuery(event);
 	const key = query.key;
 
-	if (!validKeys.includes(key)) {
-		setResponseStatus(event, 404, `Config value ${key} not found.`);
-		return '404';
+	if (!allowedKeys.includes(key)) {
+		console.error(`Config value "${key}" is not allowed to be accessed, please add it to the list of allowed keys if required.`);
+		setResponseStatus(event, 403, `Config value "${key}" is not an accessible key.`);
+		return '403';
 	}
 
 	const config = useRuntimeConfig();
+	if (!config.APP_CONFIG_ENDPOINT) {
+		console.error('APP_CONFIG_ENDPOINT env not found. Please ensure it is set.');
+		setResponseStatus(event, 500, `Configuration endpoint missing.`);
+		return '500';
+	}
 	const appConfigClient = new AppConfigurationClient(config.APP_CONFIG_ENDPOINT);
 
 	try {
 		const setting = await appConfigClient.getConfigurationSetting({ key });
 		return setting.value;
 	} catch (error) {
-		setResponseStatus(event, 404, `Config value ${key} not found.`);
+		console.error(`Failed to load config value for "${key}", please ensure it exists and is the correct format.`);
+		setResponseStatus(event, 404, `Config value "${key}" not found.`);
 		return '404';
 	}
 });
