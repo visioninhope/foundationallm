81a59c36aea93069f7820814d18dccecf4a6398f
joelhulen <joelhulen@gmail.com>
2023-11-21 11:22:53 -0500

    Update Prompt Hub request Python object to include Session ID

--- src/python/PythonSDK/foundationallm/hubs/prompt/prompt_hub_request.py
+++ src/python/PythonSDK/foundationallm/hubs/prompt/prompt_hub_request.py
@@ -6,6 +6,8 @@ class PromptHubRequest(BaseModel):
     PromptHubRequest contains the information needed to retrieve prompts from the PromptHub.
     
     agent_name: the name of the agent to retrieve prompts for, send an empty string or None to retrieve all prompts.
+    session_id: the session ID value; can be used for caching.
     """
     agent_name: Optional[str] = None
+    session_id:Optional[str] = None
     prompt_name: Optional[str] = 'default'
