64128d66df5ab71dac26e3fcccdb87db8111e73b
Andrei Alistar <andrei.alistar@genisoft.eu>
2023-11-22 16:09:05 +0200

    initial Gatekeeper Integration API

---
+++ src/python/GatekeeperIntegrationAPI/app/routers/anonymize.py
@@ -0,0 +1,30 @@
+import logging
+from fastapi import APIRouter, Depends, HTTPException
+from app.dependencies import validate_api_key_header
+
+router = APIRouter(
+    prefix='/anonymize',
+    tags=['anonymize'],
+    dependencies=[Depends(validate_api_key_header)],
+    responses={404: {'description':'Not found'}},
+    redirect_slashes=False
+)
+
+@router.post('')
+async def anonymize(text: str | None = None) -> str:
+    """
+    Anonymize text with identified PII entities.
+    
+    Returns
+    -------
+    str
+        Returns the anonymized text.
+    """
+    try:
+        return "Not implemented"
+    except Exception as e:
+        logging.error(e, stack_info=True, exc_info=True)
+        raise HTTPException(
+            status_code = 500,
+            detail = e.message
+        )
