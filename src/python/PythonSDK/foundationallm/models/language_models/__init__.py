518128f42259178c3d6de380034c3a212e5ee982
kylebunting <kyle.bunting@gmail.com>
2023-11-24 09:02:14 -0700

    Adding embedding model to language model module

--- src/python/PythonSDK/foundationallm/models/language_models/__init__.py
+++ src/python/PythonSDK/foundationallm/models/language_models/__init__.py
@@ -1,5 +1,6 @@
 """Language models module"""
 from .azure_openai_api_types import AzureOpenAIAPIType
+from .embedding_model import EmbeddingModel
 from .language_model_provider import LanguageModelProvider
 from .language_model_type import LanguageModelType
 from .language_model import LanguageModel
