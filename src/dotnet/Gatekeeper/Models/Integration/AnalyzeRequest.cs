957df4d93ae060a8bb1059241584bccc24c9de05
Andrei Alistar <andrei.alistar@genisoft.eu>
2023-12-19 21:07:47 +0200

    Add missing XML comments

--- src/dotnet/Gatekeeper/Models/Integration/AnalyzeRequest.cs
+++ src/dotnet/Gatekeeper/Models/Integration/AnalyzeRequest.cs
@@ -2,14 +2,26 @@
 
 namespace FoundationaLLM.Gatekeeper.Core.Models.Integration
 {
+    /// <summary>
+    /// Encapsulates a text analysis request.
+    /// </summary>
     public class AnalyzeRequest
     {
+        /// <summary>
+        /// The text to be analyzed.
+        /// </summary>
         [JsonProperty("content")]
         public required string Content { get; set; }
 
+        /// <summary>
+        /// A flag used to tell if PII found by analysis should be anonymized.
+        /// </summary>
         [JsonProperty("anonymize")]
         public required bool Anonymize {  get; set; }
 
+        /// <summary>
+        /// The language used to detect PII.
+        /// </summary>
         [JsonProperty("language")]
         public string? Language {  get; set; }
     }
