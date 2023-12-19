a65f70ca390e14a291f1f0caff2981e6b8964e6b
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2023-12-19 17:21:02 +0200

    Storage service abstractions

---
+++ src/dotnet/Common/Interfaces/IStorageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Common.Interfaces
+{
+    /// <summary>
+    /// Provides services to interact with a storage.
+    /// </summary>
+    public interface IStorageService
+    {
+    }
+}
