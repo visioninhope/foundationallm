47259fbc21b37c818317d291ec51afaaaa547566
joelhulen <joelhulen@gmail.com>
2024-01-09 22:06:52 -0500

    Update core worker to use app config key constants

--- src/dotnet/CoreWorker/Program.cs
+++ src/dotnet/CoreWorker/Program.cs
@@ -3,6 +3,7 @@ using FoundationaLLM.Core.Models.Configuration;
 using FoundationaLLM.Core.Interfaces;
 using FoundationaLLM.Core.Services;
 using FoundationaLLM.Core.Worker;
+using FoundationaLLM.Common.Constants;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -11,26 +12,26 @@ builder.Configuration.AddJsonFile("appsettings.json", false, true);
 builder.Configuration.AddEnvironmentVariables();
 builder.Configuration.AddAzureAppConfiguration(options =>
 {
-    options.Connect(builder.Configuration["FoundationaLLM:AppConfig:ConnectionString"]);
+    options.Connect(builder.Configuration[AppConfigurationKeys.FoundationaLLM_AppConfig_ConnectionString]);
 
     options.ConfigureKeyVault(options =>
     {
         options.SetCredential(new DefaultAzureCredential());
     });
-    options.Select("FoundationaLLM:CoreWorker:*");
-    options.Select("FoundationaLLM:CosmosDB:*");
+    options.Select(AppConfigurationKeyFilters.FoundationaLLM_CoreWorker);
+    options.Select(AppConfigurationKeyFilters.FoundationaLLM_CosmosDB);
 });
 if (builder.Environment.IsDevelopment())
     builder.Configuration.AddJsonFile("appsettings.development.json", true, true);
 
 builder.Services.AddOptions<CosmosDbSettings>()
-    .Bind(builder.Configuration.GetSection("FoundationaLLM:CosmosDB"));
+    .Bind(builder.Configuration.GetSection(AppConfigurationKeySections.FoundationaLLM_CosmosDB));
 builder.Services.AddSingleton<ICosmosDbService, CosmosDbService>();
 builder.Services.AddSingleton<ICosmosDbChangeFeedService, CosmosDbChangeFeedService>();
 builder.Services.AddHostedService<ChangeFeedWorker>();
 builder.Services.AddApplicationInsightsTelemetryWorkerService(options =>
 {
-    options.ConnectionString = builder.Configuration["FoundationaLLM:CoreWorker:AppInsightsConnectionString"];
+    options.ConnectionString = builder.Configuration[AppConfigurationKeys.FoundationaLLM_CoreWorker_AppInsightsConnectionString];
 });
 
 var host = builder.Build();
