93fec9cb94b59c2cc8dc5ee04fbca2ee85b53982
joelhulen <joelhulen@gmail.com>
2024-01-23 16:52:59 -0500

    Define service interfaces

---
+++ src/dotnet/Management/Interfaces/IDataSourceHubAPIService.cs
@@ -0,0 +1,23 @@
+﻿using FoundationaLLM.Management.Models.Configuration.Cache;
+
+namespace FoundationaLLM.Management.Interfaces
+{
+    /// <summary>
+    /// Contains base functionality for calling the DataSourceHubAPI service.
+    /// </summary>
+    public interface IDataSourceHubAPIService
+    {
+        /// <summary>
+        /// Refreshes the configuration cache.
+        /// </summary>
+        /// <returns></returns>
+        Task<APICacheRefreshResult> RefreshConfigurationCache();
+
+        /// <summary>
+        /// Refreshes the named cache.
+        /// </summary>
+        /// <param name="name">The name of the cache item to refresh.</param>
+        /// <returns></returns>
+        Task<APICacheRefreshResult> RefreshCache(string name);
+    }
+}
