4826ffbdade2f9a7bccb0cc215cc3fbe092ec1d3
joelhulen <joelhulen@gmail.com>
2024-01-27 14:57:57 -0500

    Small typo

--- src/dotnet/Vectorization/Models/Resources/ContentSource.cs
+++ src/dotnet/Vectorization/Models/Resources/ContentSource.cs
@@ -3,7 +3,7 @@
 namespace FoundationaLLM.Vectorization.Models.Resources
 {
     /// <summary>
-    /// Provides detials about a content source.
+    /// Provides details about a content source.
     /// </summary>
     public class ContentSource
     {
