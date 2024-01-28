0d4c00909b5999d171f6119026b372aa335a3a4c
joelhulen <joelhulen@gmail.com>
2024-01-27 23:16:56 -0500

    Define agent resource types

---
+++ src/dotnet/Agent/ResourceProviders/AgentResourceTypeNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Agent.ResourceProviders
+{
+    /// <summary>
+    /// Contains constants of the names of the resource types managed by the FoundationaLLM.Agent resource manager.
+    /// </summary>
+    internal class AgentResourceTypeNames
+    {
+        /// <summary>
+        /// Agent references.
+        /// </summary>
+        public const string AgentReferences = "agentreferences";
+    }
+}
