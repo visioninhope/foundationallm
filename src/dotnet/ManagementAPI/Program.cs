fba9b83581464cbd4ae784986055d9f0532adaef
Carey Payette <carey.payette@gmail.com>
2024-01-31 12:55:50 -0500

    Add alternate base path to support /management/ prefix
    Eliminates the need to use a prefix on swagger.

--- src/dotnet/ManagementAPI/Program.cs
+++ src/dotnet/ManagementAPI/Program.cs
@@ -201,6 +201,14 @@ namespace FoundationaLLM.Management.API
             // Set the CORS policy before other middleware.
             app.UseCors(allowAllCorsOrigins);
 
+            // alternate path base for the management API - serves at root and at /BASE_URL            
+            if (Environment.GetEnvironmentVariable("BASE_URL") != null)
+            {
+                var relative_path = Environment.GetEnvironmentVariable("BASE_URL");
+                relative_path = relative_path!.TrimEnd('/');
+                app.UsePathBase(new PathString(relative_path));                
+            }
+
             // For the CoreAPI, we need to make sure that UseAuthentication is called before the UserIdentityMiddleware.
             app.UseAuthentication();
             app.UseAuthorization();
@@ -213,7 +221,7 @@ namespace FoundationaLLM.Management.API
                     => await Results.Problem().ExecuteAsync(context)));
 
             // Configure the HTTP request pipeline.
-            app.UseStaticFiles();
+            
             app.UseSwagger(p => p.SerializeAsV2 = true);
             app.UseSwaggerUI(
                 options =>
@@ -225,12 +233,7 @@ namespace FoundationaLLM.Management.API
                     {
                         var url = $"/swagger/{description.GroupName}/swagger.json";
                         var name = description.GroupName.ToUpperInvariant();                       
-                        options.SwaggerEndpoint(url, name);
-                        if (Environment.GetEnvironmentVariable("BASE_URL") != null)
-                        {
-                            var relativeUrl = Environment.GetEnvironmentVariable("BASE_URL")!.Replace("/","");                            
-                            options.RoutePrefix = $"{relativeUrl}/swagger";
-                        }
+                        options.SwaggerEndpoint(url, name);                        
                     }
 
                     options.OAuthAdditionalQueryStringParams(new Dictionary<string, string>() { { "resource", builder.Configuration[AppConfigurationKeys.FoundationaLLM_Management_Entra_ClientId] } });
