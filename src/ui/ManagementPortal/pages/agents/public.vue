abedc1d229a10c58755c8a7ab15b9a891159faa1
Austin Hulen <austin@thehulens.com>
2024-02-04 22:30:55 -0800

    Imported Data Table Component and added it to the public agents page. Also Added a get agents function to the api.ts file.

--- src/ui/ManagementPortal/pages/agents/public.vue
+++ src/ui/ManagementPortal/pages/agents/public.vue
@@ -1,11 +1,36 @@
 <template>
 	<h2 class="page-header">Public Agents</h2>
 	<div class="page-subheader">View your publicly accessible agents.</div>
+	<DataTable :value="agents" stripedRows tableStyle="min-width: 50rem">
+		<Column field="name" header="Name"></Column>
+		<Column field="description" header="Description"></Column>
+	</DataTable>
 </template>
 
 <script lang="ts">
+import api from '@/js/api';
+import type Agent from '@/js/types';
+
 export default {
 	name: 'PublicAgents',
+
+	data() {
+		return {
+			agents: [] as Agent,
+		};
+	},
+
+	async created() {
+		try {
+			this.agents = await api.getAgents();
+			console.log(this.agents);
+		} catch(error) {
+			this.$toast.add({
+				severity: 'error',
+				detail: error?.response?._data || error,
+			});
+		}
+	}
 };
 </script>
 
