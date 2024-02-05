55c5804804fa3e441d8ab44418cd143b6f29c5ad
Austin Hulen <austin@thehulens.com>
2024-02-04 23:57:28 -0800

    Added more columns to data table

--- src/ui/ManagementPortal/pages/agents/public.vue
+++ src/ui/ManagementPortal/pages/agents/public.vue
@@ -2,8 +2,13 @@
 	<h2 class="page-header">Public Agents</h2>
 	<div class="page-subheader">View your publicly accessible agents.</div>
 	<DataTable :value="agents" stripedRows tableStyle="min-width: 50rem">
-		<Column field="name" header="Name"></Column>
+		<Column field="name" header="Name" sortable></Column>
 		<Column field="description" header="Description"></Column>
+		<Column field="sessions_enabled" header="Sessions Enabled" sortable></Column>
+		<Column field="prompt" header="Prompt"></Column>
+		<Column field="type" header="Type" sortable></Column>
+		<Column field="conversation_history.enabled" header="Conversation History Enabled" sortable></Column>
+		<Column field="conversation_history.max_history" header="Max Conversation History" sortable></Column>
 	</DataTable>
 </template>
 
