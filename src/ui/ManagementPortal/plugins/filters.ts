9e6a18a8390a8fed6f53db2419c40ccc57d74d71
joelhulen <joelhulen@gmail.com>
2024-02-07 14:35:24 -0500

    Do not apply the base URL when using an absolute path

--- src/ui/ManagementPortal/plugins/filters.ts
+++ src/ui/ManagementPortal/plugins/filters.ts
@@ -1,6 +1,10 @@
 const filters = {
 	publicDirectory(path: string) {
 		const config = useRuntimeConfig();
+		// Only append the base URL if the path is not an absolute URL.
+		if (path.startsWith('http')) {
+			return path;
+		}
 		return config.app.baseURL + path;
 	}
 };
