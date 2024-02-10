3d8c76e92e72860113910cb39582559093f18794
joelhulen <joelhulen@gmail.com>
2024-02-10 16:05:37 -0500

    Base validators

---
+++ src/dotnet/Common/Validation/ResourceProvider/ResourceBaseValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FoundationaLLM.Common.Models.ResourceProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Common.Validation.ResourceProvider
+{
+    /// <summary>
+    /// Validator for the <see cref="ResourceBase"/> model.
+    /// </summary>
+    public class ResourceBaseValidator : AbstractValidator<ResourceBase>
+    {
+        /// <summary>
+        /// Configures the validation rules for the <see cref="ResourceBase"/> model.
+        /// </summary>
+        public ResourceBaseValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Resource name is required.");
+            RuleFor(x => x.Type).NotEmpty().WithMessage("Resource type is required.");
+            RuleFor(x => x.ObjectId).NotEmpty().WithMessage("Resource object ID is required.");
+        }
+    }
+}
