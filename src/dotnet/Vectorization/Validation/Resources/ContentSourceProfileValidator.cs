e17aab555307bba3cc800f8df238ff08571f89c4
joelhulen <joelhulen@gmail.com>
2024-02-13 13:22:22 -0500

    Add vectorization resource validators

---
+++ src/dotnet/Vectorization/Validation/Resources/ContentSourceProfileValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using FoundationaLLM.Vectorization.Models.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Vectorization.Validation.Resources
+{
+    /// <summary>
+    /// Validator for the <see cref="ContentSourceProfile"/> model.
+    /// </summary>
+    public class ContentSourceProfileValidator : AbstractValidator<ContentSourceProfile>
+    {
+        /// <summary>
+        /// Configures the validation rules for the <see cref="ContentSourceProfile"/> model.
+        /// </summary>
+        public ContentSourceProfileValidator()
+        {
+            // Include the base validator to apply its rules
+            Include(new VectorizationProfileBaseValidator());
+
+            // Validate the Type property to ensure it's a valid enum value.
+            RuleFor(x => x.Type)
+                .IsInEnum().WithMessage("The content source type must be a valid value.");
+        }
+    }
+}
