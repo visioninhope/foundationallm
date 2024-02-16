166add098c50e44ef4f1289e5a6f77afb95b3089
Jim Counts <jamesrcounts@outlook.com>
2024-02-16 23:04:38 +0000

    Remove Hostfile support from Generate-Config

--- deploy/standard/scripts/Unified-Deploy.ps1
+++ deploy/standard/scripts/Unified-Deploy.ps1
@@ -12,6 +12,25 @@ Set-PSDebug -Trace 0 # Echo every command (0 to disable, 1 to enable)
 Set-StrictMode -Version 3.0
 $ErrorActionPreference = "Stop"
 
+function Invoke-AndRequireSuccess {
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Message,
+
+        [Parameter(Mandatory = $true, Position = 1)]
+        [ScriptBlock]$ScriptBlock
+    )
+
+    $result = & $ScriptBlock
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed: ${message}" -ForegroundColor Red
+        exit $LASTEXITCODE
+    }
+
+    return $result
+}
+
 Push-Location $($MyInvocation.InvocationName | Split-Path)
 
 if ($init) {
@@ -56,6 +75,11 @@ if ($stepUploadSystemPrompts) {
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Error generating config" -ForegroundColor Red
     exit $LASTEXITCODE
+}
+
+Write-Host "Generate Host File" -ForegroundColor Blue
+Invoke-AndRequireSuccess "Generate Host File" {
+    & ./Generate-Hosts.ps1 -subscription $manifest.subscription
 }
 
 if ($stepDeployCerts) {
