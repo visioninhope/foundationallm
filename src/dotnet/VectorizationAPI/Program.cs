a42f7d35116e5c88470748e6ce11c96b07cb39b7
joelhulen <joelhulen@gmail.com>
2024-02-16 16:13:27 -0500

    Update API versioning

--- src/dotnet/VectorizationAPI/Program.cs
+++ src/dotnet/VectorizationAPI/Program.cs
@@ -168,19 +168,15 @@ builder.Services.AddOptions<APIKeyValidationSettings>()
 
 builder.Services
     .AddApiVersioning(options =>
-     {
-         // Reporting api versions will return the headers
-         // "api-supported-versions" and "api-deprecated-versions"
-         options.ReportApiVersions = true;
-         options.AssumeDefaultVersionWhenUnspecified = true;
-         options.DefaultApiVersion = new ApiVersion(1, 0);
-     })
-    .AddApiExplorer(options =>
     {
-        // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
-        // note: the specified format code will format the version as "'v'major[.minor][-status]"
-        options.GroupNameFormat = "'v'VVV";
-    });
+        // Reporting api versions will return the headers
+        // "api-supported-versions" and "api-deprecated-versions"
+        options.ReportApiVersions = true;
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.DefaultApiVersion = new ApiVersion(new DateOnly(2024, 2, 16));
+    })
+    .AddMvc()
+    .AddApiExplorer();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
