820b6aba463205e9173dd9706edb92631ebf6726
kylebunting <kyle.bunting@gmail.com>
2024-02-29 18:34:53 -0700

    Removing agent hint from DataSourceHub

--- src/python/PythonSDK/foundationallm/hubs/data_source/data_source_hub_storage_manager.py
+++ src/python/PythonSDK/foundationallm/hubs/data_source/data_source_hub_storage_manager.py
@@ -9,22 +9,21 @@ from foundationallm.storage import BlobStorageManager
 class DataSourceHubStorageManager(BlobStorageManager):
     """
     The DataSourceHubStorageManager class is responsible for fetching available datasource
-        values from Azure Blob Storage.
+    values from Azure Blob Storage.
     """
-    def __init__(self, prefix:str=None, config: Configuration = None):
-        connection_string = config.get_value(
-                    "FoundationaLLM:DataSourceHub:StorageManager:BlobStorage:ConnectionString"
-                    )
-        container_name = config.get_value(
-                    "FoundationaLLM:DataSourceHub:DataSourceMetadata:StorageContainer"
-                    )
-        if prefix is not None:
-            container_name = f"{prefix}/{container_name}"
-        super().__init__(blob_connection_string=connection_string,
-                            container_name=container_name)
+    def __init__(self, config: Configuration = None):
+        connection_string = config.get_value("FoundationaLLM:DataSourceHub:StorageManager:BlobStorage:ConnectionString")
+        container_name = config.get_value("FoundationaLLM:DataSourceHub:DataSourceMetadata:StorageContainer")
+        
+        super().__init__(
+            blob_connection_string=connection_string,
+            container_name=container_name
+        )
 
     def read_file_content(self, path) -> str:
-        return super().read_file_content(path).decode()
+        file_content = super().read_file_content(path)
+        if file_content is not None:
+            return file_content.decode()
 
     def list_blobs(self):
         blob_list: List[dict] = list(super().list_blobs(path=""))
