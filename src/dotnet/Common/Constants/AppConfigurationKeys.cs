93ceb814be42b0796e63e2f34d27bed0356c49fa
joelhulen <joelhulen@gmail.com>
2024-03-01 11:23:21 -0500

    Remove Allow Agent Selection feature flag

--- src/dotnet/Common/Constants/AppConfigurationKeys.cs
+++ src/dotnet/Common/Constants/AppConfigurationKeys.cs
@@ -765,17 +765,6 @@ namespace FoundationaLLM.Common.Constants
     }
 
     /// <summary>
-    /// Contains constants of the feature flags for app configuration settings.
-    /// </summary>
-    public static class AppConfigurationFeatureFlags
-    {
-        /// <summary>
-        /// This feature flag controls whether the User Portal UI allows users to select an agent hint.
-        /// </summary>
-        public const string FoundationaLLM_AllowAgentHint = "FoundationaLLM-AllowAgentHint";
-    }
-
-    /// <summary>
     /// Contains constants of the keys filters for app configuration setting namespaces.
     /// </summary>
     public static class AppConfigurationKeyFilters
