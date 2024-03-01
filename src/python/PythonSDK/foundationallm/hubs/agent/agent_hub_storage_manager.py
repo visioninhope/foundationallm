c21e462244abdb29e6946dd88a60c3f459cf0312
kylebunting <kyle.bunting@gmail.com>
2024-02-29 18:33:41 -0700

    Removing agent hint from AgentHub

--- src/python/PythonSDK/foundationallm/hubs/agent/agent_hub_storage_manager.py
+++ src/python/PythonSDK/foundationallm/hubs/agent/agent_hub_storage_manager.py
@@ -6,20 +6,23 @@ from foundationallm.config import Configuration
 from foundationallm.storage import BlobStorageManager
 
 class AgentHubStorageManager(BlobStorageManager):
-    """ The AgentHubStorageManager class is responsible for fetching
-        available agent values from Azure Blob Storage."""
-    def __init__(self, prefix:str = None, config: Configuration = None):
-        connection_string = config.get_value(
-            "FoundationaLLM:AgentHub:StorageManager:BlobStorage:ConnectionString")
+    """
+    The AgentHubStorageManager class is responsible for fetching
+    available agent values from Azure Blob Storage.
+    """
+    def __init__(self, config: Configuration = None):
+        connection_string = config.get_value("FoundationaLLM:AgentHub:StorageManager:BlobStorage:ConnectionString")
         container_name = config.get_value("FoundationaLLM:AgentHub:AgentMetadata:StorageContainer")
-        if prefix is not None:
-            container_name = f"{prefix}/{container_name}"
 
-        super().__init__(blob_connection_string=connection_string,
-                            container_name=container_name)
+        super().__init__(
+            blob_connection_string=connection_string,
+            container_name=container_name
+        )
 
     def read_file_content(self, path) -> str:
-        return super().read_file_content(path).decode()
+        file_content = super().read_file_content(path)
+        if file_content is not None:
+            return file_content.decode()
 
     def list_blobs(self, path):
         blob_list: List[dict] = list(super().list_blobs(path=path))
--- src/python/PythonSDK/foundationallm/hubs/agent/agent_resolver.py
+++ src/python/PythonSDK/foundationallm/hubs/agent/agent_resolver.py
@@ -4,31 +4,11 @@ The AgentResolver class is responsible for resolving a request to a metadata val
 from foundationallm.config import Context
 from foundationallm.hubs import Resolver
 from foundationallm.hubs.agent import AgentHubRequest, AgentHubResponse
-from foundationallm.models import AgentHint
 
 class AgentResolver(Resolver):
     """
     The AgentResolver class is responsible for resolving a request to a metadata value.
-    
-    If a hint is provided and the FoundationaLLM-AllowAgentHint flag enabled,
-        the resolver will return the agent with that name.
-    If a hinted agent is not found, return the default agent.
     """
-    def resolve(self, request:AgentHubRequest,
-                user_context:Context=None, hint:AgentHint=None) -> AgentHubResponse:
-        hint_feature_flag_enabled = self.config.get_feature_flag("FoundationaLLM-AllowAgentHint")
-        agent_metadata = None
-        if hint_feature_flag_enabled and hint is not None:
-            path_prefix = None
-            if hint.private:
-                # build the container prefix based on the user identity
-                user_upn = user_context.user_identity.upn.replace("@", "_")
-                path_prefix = f"user-profiles/{user_upn}/"
-                # override the container prefix for private agent lookup
-                self.repository.container_prefix = path_prefix
-            agent_metadata = self.repository.get_metadata_by_name(name=hint.name)
-            # Reset the container prefix for global agent (if necessary)
-            self.repository.container_prefix = None
-        if agent_metadata is None:
-            agent_metadata = self.repository.get_metadata_by_name("default")
+    def resolve(self, request:AgentHubRequest, user_context:Context=None) -> AgentHubResponse:
+        agent_metadata = self.repository.get_metadata_by_name('default')
         return AgentHubResponse(agent=agent_metadata)
