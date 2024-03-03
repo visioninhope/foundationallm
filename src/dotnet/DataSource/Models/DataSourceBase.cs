287e4dc867db2100e32c0f32976bac2e2b73e65a
Ciprian Jichici <ciprian@solliance.net>
2024-03-03 21:07:34 +0200

    Data Source base class

---
+++ src/dotnet/DataSource/Models/DataSourceBase.cs
@@ -0,0 +1,16 @@
+﻿using FoundationaLLM.Common.Models.ResourceProvider;
+using System.Text.Json.Serialization;
+
+namespace FoundationaLLM.DataSource.Models
+{
+    /// <summary>
+    /// Basic data source.
+    /// </summary>
+    [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+    public class DataSourceBase : ResourceBase
+    {
+        /// <inheritdoc/>
+        [JsonIgnore]
+        public override string? Type { get; set; }
+    }
+}
