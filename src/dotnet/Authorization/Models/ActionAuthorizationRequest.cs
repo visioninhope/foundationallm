c8ce42efed2a99270886b0fa24a9a05b07b5e0f7
Ciprian Jichici <ciprian@solliance.net>
2024-03-10 15:52:09 +0200

    Authorization controller in authorization API

---
+++ src/dotnet/Authorization/Models/ActionAuthorizationRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoundationaLLM.Authorization.Models
+{
+    /// <summary>
+    /// Represents a request to authorize an action.
+    /// </summary>
+    public class ActionAuthorizationRequest
+    {
+        /// <summary>
+        /// The FoundationaLLM instance id to which the request is targeted.
+        /// </summary>
+        public required string InstanceId { get; set; }
+
+        /// <summary>
+        /// The authorizable action for which authorization is requested.
+        /// </summary>
+        [JsonPropertyName("action")]
+        public required string Action { get; set; }
+
+        /// <summary>
+        /// The resource for which authorization is requested.
+        /// </summary>
+        [JsonPropertyName("resource")]
+        public required string Resource { get; set; }
+
+        /// <summary>
+        /// The id of the security principal requesting authorization.
+        /// </summary>
+        [JsonPropertyName("principal_id")]
+        public required string PrincipalId { get; set; }
+
+        /// <summary>
+        /// The list of security group ids to which the principal belongs.
+        /// </summary>
+        [JsonPropertyName("security_group_ids")]
+        public List<string> SecurityGroupIds { get; set; } = [];    
+    }
+}
