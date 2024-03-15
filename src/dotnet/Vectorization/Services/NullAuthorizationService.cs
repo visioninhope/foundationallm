9b4e09865d2402c1cef219c6af6a44467ddf0a2f
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2024-03-15 23:28:31 +0200

    Fix authorization initialization in Vectorization Worker

--- src/dotnet/Vectorization/Services/NullAuthorizationService.cs
+++ src/dotnet/Vectorization/Services/NullAuthorizationService.cs
@@ -3,8 +3,12 @@ using FoundationaLLM.Common.Models.Authorization;
 
 namespace FoundationaLLM.Vectorization.Services
 {
+    /// <summary>
+    /// Implements an authorization service that bypasses the Authorization API and allows all access by default.
+    /// </summary>
     public class NullAuthorizationService : IAuthorizationService
     {
+        /// <inheritdoc/>
         public async Task<ActionAuthorizationResult> ProcessAuthorizationRequest(ActionAuthorizationRequest authorizationRequest)
         {
             await Task.CompletedTask;
