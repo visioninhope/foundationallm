a29b52a257ca6ce0fde92a1cd875f997798057c0
Ciprian Jichici <ciprian@solliance.net>
2024-03-28 19:19:28 +0200

    Pipeline execution core implementation

---
+++ src/dotnet/Vectorization/Interfaces/IPipelineExecutionService.cs
@@ -0,0 +1,22 @@
+﻿namespace FoundationaLLM.Vectorization.Interfaces
+{
+    /// <summary>
+    /// Provides services to interact with the pipeline execution service.
+    /// </summary>
+    public interface IPipelineExecutionService
+    {
+        /// <summary>
+        /// Executes the pipeline execution service until cancellation is signaled.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> signaling cancellation.</param>
+        /// <returns></returns>
+        Task ExecuteAsync(CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Stops the pipeline execution service, allowing it to cleanup.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> signaling cancellation.</param>
+        /// <returns></returns>
+        Task StopAsync(CancellationToken cancellationToken);
+    }
+}
