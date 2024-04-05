b8c0fb2ead6fd189fb66adf268e56eb052f5798f
Carey Payette <carey.payette@gmail.com>
2024-04-05 12:12:03 -0400

    Introduced Citation class
    This class holds source/citation information for a completion request.

--- src/python/PythonSDK/foundationallm/models/orchestration/__init__.py
+++ src/python/PythonSDK/foundationallm/models/orchestration/__init__.py
@@ -1,5 +1,6 @@
 """FoundationaLLM orchestration models module"""
 from .message_history_item import MessageHistoryItem
+from .citation import Citation
 from .orchestration_settings import OrchestrationSettings
 from .completion_request_base import CompletionRequestBase
 from .completion_request import CompletionRequest
