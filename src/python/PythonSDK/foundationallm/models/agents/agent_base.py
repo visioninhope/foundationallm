5a70aee3840cb66b920bb10bd55f21ba5b68f613
kylebunting <kyle.bunting@gmail.com>
2024-04-10 17:23:51 -0600

    Updating agent_base model + refactoring to move objects into agents directory

--- src/python/PythonSDK/foundationallm/models/agents/agent_base.py
+++ src/python/PythonSDK/foundationallm/models/agents/agent_base.py
@@ -1,16 +1,19 @@
 """
-Class Name: AgentBase
-
-Description: Encapsulates the common metadata for the agent
-fulfilling the orchestration request.
+Base Agent fulfilling the orchestration request.
 """
+from pydantic import BaseModel
 from typing import Optional
-from foundationallm.models.language_models import LanguageModel
-from foundationallm.models.metadata import ConversationHistory, Gatekeeper, MetadataBase
+from foundationallm.models.orchestration import OrchestrationSettings
+from foundationallm.models.agents import AgentConversationHistorySettings, AgentGatekeeperSettings
 
-class AgentBase(MetadataBase):
-    """ Agent Base metadata model."""
+class AgentBase(BaseModel):
+    """ Agent Base model."""
+    name: str
+    description: str
+    type: str
+    object_id: Optional[str] = None
     prompt_object_id: Optional[str] = None
     sessions_enabled: Optional[bool] = False
-    conversation_history: Optional[ConversationHistory] = ConversationHistory()
-    gatekeeper: Optional[Gatekeeper] = Gatekeeper()
+    conversation_history: Optional[AgentConversationHistorySettings] = AgentConversationHistorySettings()
+    gatekeeper: Optional[AgentGatekeeperSettings] = AgentGatekeeperSettings()
+    orchestration_settings: Optional[OrchestrationSettings] = OrchestrationSettings()
