73cd5f870a1ad62c476653cde980943cdc22b910
joelhulen <joelhulen@gmail.com>
2024-04-10 02:31:15 -0400

    Create new chat session if needed, then rename it

--- tests/dotnet/Core.Examples/Example01_AgentCompletions.cs
+++ tests/dotnet/Core.Examples/Example01_AgentCompletions.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Runtime;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using FoundationaLLM.Common.Authentication;
 using FoundationaLLM.Common.Constants;
@@ -42,9 +43,29 @@ namespace FoundationaLLM.Core.Examples
 		public async Task RunExampleAsync()
 		{
 			// Arrange
+			var createNewSession = true;
 			var cosmosDbSettings = TestConfiguration.CosmosDbSettings;
 			var client = await GetHttpClient();
-			var sessionId = Guid.NewGuid().ToString();
+			var sessionId = ""; // If you are not creating a new chat session, enter the existing session ID here.
+			if (createNewSession)
+			{
+				var responseSession = await client.PostAsync("sessions", null);
+
+				if (responseSession.IsSuccessStatusCode)
+				{
+					var responseContent = await responseSession.Content.ReadAsStringAsync();
+					var sessionResponse = JsonSerializer.Deserialize<Session>(responseContent, _jsonSerializerOptions);
+					sessionId = sessionResponse.SessionId;
+
+					var sessionName = "Test: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+					var responseUpdate = await client.PostAsync($"sessions/{sessionId}/rename?newChatSessionName={UrlEncoder.Default.Encode(sessionName)}", null);
+
+					if (!responseUpdate.IsSuccessStatusCode)
+					{
+						WriteLine("Failed to update the session name.");
+					}
+				}
+			}
 			var agentName = "FoundationaLLM";
 			var userPrompt = "Hello, who are you?";
 			var expectedCompletion = "I am an analytic agent named Khalil. How may I assist you?";
@@ -59,7 +80,10 @@ namespace FoundationaLLM.Core.Examples
 			};
 
 			var serializedRequest = JsonSerializer.Serialize(orchestrationRequest, _jsonSerializerOptions);
-			var responseMessage = await client.PostAsync("orchestration/completion",
+			
+			var orchestrationUrl = "orchestration/completion"; // Sessionless - no message history and data is not retained in Cosmos DB.
+			var sessionUrl = $"sessions/{sessionId}/completion"; // Session-based - message history and data is retained in Cosmos DB. Must create a session if it does not exist.
+			var responseMessage = await client.PostAsync(sessionUrl,
 				new StringContent(
 					serializedRequest,
 					Encoding.UTF8, "application/json"));
