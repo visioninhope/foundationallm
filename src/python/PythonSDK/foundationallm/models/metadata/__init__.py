adff5e0706ff282779eff901d1cb3c47f0418dc0
kylebunting <kyle.bunting@gmail.com>
2024-04-10 17:25:41 -0600

    Deleting legacy objects

--- src/python/PythonSDK/foundationallm/models/metadata/__init__.py
+++
@@ -1,6 +0,0 @@
-"""FoundationaLLM metadata models module"""
-from .agent import Agent
-from .conversation_history import ConversationHistory
-from .data_source import DataSource
-from .gatekeeper import Gatekeeper
-from .metadata_base import MetadataBase
