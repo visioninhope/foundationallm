82a2845371e9d2ffed5f0cb7ddf89fc235112a5b
joelhulen <joelhulen@gmail.com>
2024-04-12 16:56:16 -0400

    Implement graph group paging and set the page size to a higher value

--- src/dotnet/Common/Services/Security/MicrosoftGraphGroupMembershipService.cs
+++ src/dotnet/Common/Services/Security/MicrosoftGraphGroupMembershipService.cs
@@ -1,6 +1,7 @@
 ﻿using FoundationaLLM.Common.Authentication;
 using FoundationaLLM.Common.Interfaces;
 using Microsoft.Graph;
+using System.Collections.Generic;
 
 namespace FoundationaLLM.Common.Services.Security
 {
@@ -15,10 +16,36 @@ namespace FoundationaLLM.Common.Services.Security
         /// <inheritdoc/>
         public async Task<List<string>> GetGroupsForPrincipal(string userPrincipalName)
         {
-            var result = await _graphClient.Users[userPrincipalName].TransitiveMemberOf.GraphGroup.GetAsync().ConfigureAwait(false);
-            return result == null || result.Value == null
+            var groupMembership = new List<Microsoft.Graph.Models.Group>();
+            var groups = await _graphClient.Users[userPrincipalName].TransitiveMemberOf.GraphGroup.GetAsync(requestConfiguration =>
+            {
+                requestConfiguration.QueryParameters.Top = 500;
+            }).ConfigureAwait(false);
+
+            while (groups?.Value != null)
+            {
+                foreach (var group in groups.Value)
+                {
+                    Console.WriteLine(group.DisplayName);
+                    groupMembership.Add(group);
+                }
+
+                // Invoke paging if required.
+                if (!string.IsNullOrEmpty(groups.OdataNextLink))
+                {
+                    groups = await _graphClient.Users[userPrincipalName].TransitiveMemberOf.GraphGroup
+                        .WithUrl(groups.OdataNextLink)
+                        .GetAsync();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return groupMembership.Count == 0
                 ? []
-                : result.Value.Where(x => x.Id != null).Select(x => x.Id!).ToList();
+                : groupMembership.Where(x => x.Id != null).Select(x => x.Id!).ToList();
         }
     }
 }
