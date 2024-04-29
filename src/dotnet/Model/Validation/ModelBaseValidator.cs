ae2bc1ee9079a17934fed37fe5c2dea6e6f7011c
Kyle Bunting <kyle@solliance.net>
2024-04-29 14:21:00 -0600

    Adding validator for ModelBase class

---
+++ src/dotnet/Model/Validation/ModelBaseValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FoundationaLLM.Common.Models.ResourceProviders.Model;
+using FoundationaLLM.Common.Validation.ResourceProvider;
+
+namespace FoundationaLLM.Model.Validation
+{
+    /// <summary>
+    /// Validator for the <see cref="ModelBase"/> model.
+    /// </summary>
+    public class ModelBaseValidator : AbstractValidator<ModelBase>
+    {
+        /// <summary>
+        /// Configures the validation rules for the <see cref="ModelBase"/> model.
+        /// </summary>
+        public ModelBaseValidator() =>
+            Include(new ResourceBaseValidator());
+    }
+}
