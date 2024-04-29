c95d448b42f9986cfdd544b3d5246d832ea72dea
Kyle Bunting <kyle@solliance.net>
2024-04-29 14:20:12 -0600

    Adding ModelTypes ResourceProvider constants

---
+++ src/dotnet/Common/Constants/ResourceProviders/ModelTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace FoundationaLLM.Common.Constants.ResourceProviders
+{
+    /// <summary>
+    /// String constants for model types.
+    /// </summary>
+    public static class ModelTypes
+    {
+        /// <summary>
+        /// Basic model with practical functionality. Used as base for all other models.
+        /// </summary>
+        public const string Basic = "basic";
+    }
+}
