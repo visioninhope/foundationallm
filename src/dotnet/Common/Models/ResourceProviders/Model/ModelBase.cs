8018a5ba89576916ab1c355009c7011a78dc0a6a
Kyle Bunting <kyle@solliance.net>
2024-04-29 14:20:30 -0600

    Adding ModelBase class to resource providers

---
+++ src/dotnet/Common/Models/ResourceProviders/Model/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoundationaLLM.Common.Models.ResourceProviders.Model
+{
+    /// <summary>
+    /// Basic model.
+    /// </summary>
+    [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+    public class ModelBase : ResourceBase
+    {
+        /// <inheritdoc/>
+        [JsonIgnore]
+        public override string? Type { get; set; }
+    }
+}
