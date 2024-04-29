00629a51459640d92473d103418368eb38c9ebf2
joelhulen <joelhulen@gmail.com>
2024-04-29 19:37:39 -0400

    Add comments and account for enums in App Config section method

--- tests/dotnet/Core.Examples/Setup/TestConfiguration.cs
+++ tests/dotnet/Core.Examples/Setup/TestConfiguration.cs
@@ -15,21 +15,23 @@ using FoundationaLLM.Common.Constants;
 using FoundationaLLM.Common.Constants.Configuration;
 using FoundationaLLM.Common.Models.Configuration.AzureAI;
 using FoundationaLLM.Common.Models.Configuration.CosmosDB;
+using FoundationaLLM.Common.Models.Configuration.Storage;
 using FoundationaLLM.Common.Settings;
 using FoundationaLLM.Core.Examples.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FoundationaLLM.Core.Examples.Setup
 {
+    /// <summary>
+    /// Configures the test services for the examples.
+    /// </summary>
     public sealed class TestConfiguration
     {
         private readonly IConfigurationRoot _configRoot;
         private static TestConfiguration? _instance;
         private static ConfigurationClient? _client;
         private readonly ChainedTokenCredential _tokenCredential;
-        public static CosmosDbSettings? CosmosDbSettings;
         public static CompletionQualityMeasurementConfiguration CompletionQualityMeasurementConfiguration => LoadSection<CompletionQualityMeasurementConfiguration>();
-        public static AzureAISettings AzureAISettings => LoadSection<AzureAISettings>();
 
 		private TestConfiguration(IConfigurationRoot configRoot)
         {
@@ -40,6 +42,11 @@ namespace FoundationaLLM.Core.Examples.Setup
                 new DefaultAzureCredential());
         }
 
+        /// <summary>
+        /// Initializes the test configuration.
+        /// </summary>
+        /// <param name="configRoot"></param>
+        /// <param name="services"></param>
         public static void Initialize(IConfigurationRoot configRoot, IServiceCollection services)
         {
             _instance = new TestConfiguration(configRoot);
@@ -47,9 +54,12 @@ namespace FoundationaLLM.Core.Examples.Setup
             var connectionString =
                 _instance._configRoot.GetValue<string>(EnvironmentVariables.FoundationaLLM_AppConfig_ConnectionString);
             _client = new ConfigurationClient(connectionString);
-            CosmosDbSettings = GetAppConfigSectionAsync<CosmosDbSettings>(AppConfigurationKeyFilters.FoundationaLLM_CosmosDB).GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// Gets the token credential for the test configuration.
+        /// </summary>
+        /// <returns></returns>
         public static TokenCredential GetTokenCredential()
         {
 	        ValidateInstance();
@@ -57,6 +67,14 @@ namespace FoundationaLLM.Core.Examples.Setup
 	        return _instance!._tokenCredential;
         }
 
+        /// <summary>
+        /// Loads a section from the configuration. The source is typically the testsettings.json file.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="caller"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ConfigurationNotFoundException"></exception>
         private static T LoadSection<T>([CallerMemberName] string? caller = null)
         {
 			ValidateInstance();
@@ -70,6 +88,11 @@ namespace FoundationaLLM.Core.Examples.Setup
                    throw new ConfigurationNotFoundException(section: caller);
         }
 
+        /// <summary>
+        /// Resolves a single App Config value.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static async Task<string> GetAppConfigValueAsync(string key)
         {
 			ValidateInstance();
@@ -89,45 +112,58 @@ namespace FoundationaLLM.Core.Examples.Setup
 			return response.Value.Value;
         }
 
-        public static async Task<T> GetAppConfigSectionAsync<T>(string keyFilter)
-        {
+		/// <summary>
+		/// Loads a section from App Config.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="keyFilter"></param>
+		/// <returns></returns>
+		public static async Task<T> GetAppConfigSectionAsync<T>(string keyFilter)
+		{
 			ValidateInstance();
 
 			var selector = new SettingSelector { KeyFilter = keyFilter };
 
-            var instance = Activator.CreateInstance<T>();
+			var instance = Activator.CreateInstance<T>();
 
-            if (_client == null) return instance;
-            await foreach (var setting in _client.GetConfigurationSettingsAsync(selector))
-            {
-	            string value;
-	            if (setting is SecretReferenceConfigurationSetting secretReference)
-	            {
-		            var identifier = new KeyVaultSecretIdentifier(secretReference.SecretId);
-		            var secretClient = new SecretClient(identifier.VaultUri, _instance!._tokenCredential);
-		            var secret = await secretClient.GetSecretAsync(identifier.Name, identifier.Version);
+			if (_client == null) return instance;
+			await foreach (var setting in _client.GetConfigurationSettingsAsync(selector))
+			{
+				string value;
+				if (setting is SecretReferenceConfigurationSetting secretReference)
+				{
+					var identifier = new KeyVaultSecretIdentifier(secretReference.SecretId);
+					var secretClient = new SecretClient(identifier.VaultUri, _instance!._tokenCredential);
+					var secret = await secretClient.GetSecretAsync(identifier.Name, identifier.Version);
 
-		            value = secret.Value.Value;
-	            }
-	            else
-	            {
-		            value = setting.Value;
-	            }
+					value = secret.Value.Value;
+				}
+				else
+				{
+					value = setting.Value;
+				}
 
-	            var propertyKey = setting.Key.Split(':').Last();
+				var propertyKey = setting.Key.Split(':').Last();
+				var property = typeof(T).GetProperty(propertyKey);
+				if (property != null && property.CanWrite)
+				{
+					object convertedValue;
+					if (property.PropertyType.IsEnum)
+					{
+						convertedValue = Enum.Parse(property.PropertyType, value, ignoreCase: true);
+					}
+					else
+					{
+						convertedValue = Convert.ChangeType(value, property.PropertyType);
+					}
+					property.SetValue(instance, convertedValue);
+				}
+			}
 
-	            var property = typeof(T).GetProperty(propertyKey);
-	            if (property != null && property.CanWrite)
-	            {
-		            var convertedValue = Convert.ChangeType(value, property.PropertyType);
-		            property.SetValue(instance, convertedValue);
-	            }
-            }
+			return instance;
+		}
 
-            return instance;
-        }
-
-        private static void ValidateInstance()
+		private static void ValidateInstance()
         {
 	        if (_instance == null)
 	        {
