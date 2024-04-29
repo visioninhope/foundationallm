2f8eb4242147fbc42e88f6eb28244f567c6055e2
Kyle Bunting <kyle@solliance.net>
2024-04-29 16:24:03 -0600

    Creating EndpointBase ResourceProvider class

---
+++ src/dotnet/Common/Models/ResourceProviders/Endpoint/EndpointBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoundationaLLM.Common.Models.ResourceProviders.Endpoint
+{
+    /// <summary>
+    /// Basic endpoint.
+    /// </summary>
+    [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+    public class EndpointBase : ResourceBase
+    {
+        /// <inheritdoc/>
+        [JsonIgnore]
+        public override string? Type { get; set; }
+    }
+}
