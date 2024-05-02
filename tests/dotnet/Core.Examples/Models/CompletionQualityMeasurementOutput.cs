18a23e00ba2eee3fed8cee1ddd9620ad23653ffb
joelhulen <joelhulen@gmail.com>
2024-05-02 14:08:13 -0400

    Model for storing quality measurements test run

---
+++ tests/dotnet/Core.Examples/Models/CompletionQualityMeasurementOutput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Core.Examples.Models
+{
+    /// <summary>
+    /// Contains the output of the completion quality measurement job run.
+    /// </summary>
+    public class CompletionQualityMeasurementOutput
+    {
+        /// <summary>
+        /// Azure AI evaluation Job ID.
+        /// </summary>
+        public Guid? JobID { get; set; }
+        /// <summary>
+        /// The agent prompt that was used.
+        /// </summary>
+        public string? UserPrompt { get; set; }
+        /// <summary>
+        /// The agent completion that was generated.
+        /// </summary>
+        public string? AgentCompletion { get; set; }
+        /// <summary>
+        /// The agent completion that was expected.
+        /// </summary>
+        public string? ExpectedCompletion { get; set; }
+    }
+}
