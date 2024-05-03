b466c328c906d652d1e7ebea2460a7e61779b019
joelhulen <joelhulen@gmail.com>
2024-05-03 13:08:13 -0400

    Common constants for resource provider actions

---
+++ src/dotnet/Common/Constants/ResourceProviders/BaseResourceProviderActions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Common.Constants.ResourceProviders
+{
+    /// <summary>
+    /// Actions that can be performed by the base resource provider and any of its children.
+    /// </summary>
+    public static class BaseResourceProviderActions
+    {
+        /// <summary>
+        /// Purges a soft-deleted resource.
+        /// </summary>
+        public const string Purge = "purge";
+    }
+}
