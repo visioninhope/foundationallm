a2dadf916f8811ab8b5c941da5f9bd07fbc8970b
joelhulen <joelhulen@gmail.com>
2024-05-03 13:08:51 -0400

    Add file deletion

--- src/dotnet/Common/Services/Storage/DataLakeStorageService.cs
+++ src/dotnet/Common/Services/Storage/DataLakeStorageService.cs
@@ -180,6 +180,26 @@ namespace FoundationaLLM.Common.Services.Storage
                 cancellationToken).ConfigureAwait(false);
 
         /// <inheritdoc/>
+        public async Task DeleteFileAsync(
+            string containerName,
+            string filePath,
+            CancellationToken cancellationToken = default)
+        {
+            var fileSystemClient = _dataLakeClient.GetFileSystemClient(containerName);
+            var fileClient = fileSystemClient.GetFileClient(filePath);
+
+            try
+            {
+                await fileClient.DeleteIfExistsAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+            }
+            catch (RequestFailedException e) when (e.Status == 404)
+            {
+                _logger.LogWarning("File not found: {FilePath}", filePath);
+                throw new ContentException("File not found.", e);
+            }
+        }
+
+        /// <inheritdoc/>
         public async Task<bool> FileExistsAsync(
             string containerName,
             string filePath,
