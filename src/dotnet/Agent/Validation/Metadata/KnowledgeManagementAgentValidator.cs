6c41c1a0089c3d50d03ae32b9073c2743bdbbd98
joelhulen <joelhulen@gmail.com>
2024-05-04 11:25:46 -0400

    Add conditions for inline context agents

--- src/dotnet/Agent/Validation/Metadata/KnowledgeManagementAgentValidator.cs
+++ src/dotnet/Agent/Validation/Metadata/KnowledgeManagementAgentValidator.cs
@@ -22,12 +22,12 @@ namespace FoundationaLLM.Agent.Validation.Metadata
                     .WithMessage("Indexing profile is required for Knowledge Management Agents.");
                 RuleFor(x => x.Vectorization.TextEmbeddingProfileObjectId).NotEmpty()
                     .WithMessage("Embedding profile is required for Knowledge Management Agents.");
-            });
 
-            When(x => x.Vectorization.DedicatedPipeline, () =>
-            {
-                RuleFor(x => x.Vectorization.DataSourceObjectId).NotEmpty().WithMessage("Data source is required for Knowledge Management Agents with a dedicated pipeline.");
-                RuleFor(x => x.Vectorization.VectorizationDataPipelineObjectId).NotEmpty().WithMessage("Vectorization data pipeline is required for Knowledge Management Agents with a dedicated pipeline.");
+                When(x => x.Vectorization.DedicatedPipeline, () =>
+                {
+                    RuleFor(x => x.Vectorization.DataSourceObjectId).NotEmpty().WithMessage("Data source is required for Knowledge Management Agents with a dedicated pipeline.");
+                    RuleFor(x => x.Vectorization.VectorizationDataPipelineObjectId).NotEmpty().WithMessage("Vectorization data pipeline is required for Knowledge Management Agents with a dedicated pipeline.");
+                });
             });
         }
     }
