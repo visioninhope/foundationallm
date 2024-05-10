0359aaa1b7bdb2b621f792679dabba3f459d9619
saimachi <saikmachi@gmail.com>
2024-05-10 15:16:29 -0400

    Read group assignments for service principals.

---
+++ src/dotnet/Common/Services/Security/Graph/GraphPrincipalWithGroupsBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Graph;
+
+namespace FoundationaLLM.Common.Services.Security.Graph
+{
+    public abstract class GraphPrincipalWithGroupsBase
+    {
+        protected readonly GraphServiceClient _graphServiceClient;
+        public GraphPrincipalWithGroupsBase(GraphServiceClient graphServiceClient) => _graphServiceClient = graphServiceClient;
+    }
+}
---
+++ src/dotnet/Common/Services/Security/Graph/ServicePrincipals.cs
@@ -0,0 +1,43 @@
+﻿using FoundationaLLM.Common.Interfaces;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+
+namespace FoundationaLLM.Common.Services.Security.Graph
+{
+    public class ServicePrincipals : GraphPrincipalWithGroupsBase, IGraphPrincipalWithGroups
+    {
+        public ServicePrincipals(GraphServiceClient graphServiceClient) : base(graphServiceClient) { }
+
+        public async Task<List<Group>> GetGroups(string principalId)
+        {
+            var groups = await _graphServiceClient.ServicePrincipals[principalId].TransitiveMemberOf.GraphGroup.GetAsync(requestConfiguration =>
+            {
+                requestConfiguration.QueryParameters.Top = 500;
+            }).ConfigureAwait(false);
+
+            var groupMembership = new List<Group>();
+
+            while (groups?.Value != null)
+            {
+                foreach (var group in groups.Value)
+                {
+                    groupMembership.Add(group);
+                }
+
+                // Invoke paging if required.
+                if (!string.IsNullOrEmpty(groups.OdataNextLink))
+                {
+                    groups = await _graphServiceClient.ServicePrincipals[principalId].TransitiveMemberOf.GraphGroup
+                        .WithUrl(groups.OdataNextLink)
+                        .GetAsync();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return groupMembership;
+        }
+    }
+}
