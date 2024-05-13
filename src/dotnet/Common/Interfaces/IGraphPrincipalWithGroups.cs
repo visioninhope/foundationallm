546f792265a148267eebc53fca58d6503f5b4a19
saimachi <saikmachi@gmail.com>
2024-05-12 20:09:10 -0400

    Improved documentation.

--- src/dotnet/Common/Interfaces/IGraphPrincipalWithGroups.cs
+++ src/dotnet/Common/Interfaces/IGraphPrincipalWithGroups.cs
@@ -4,6 +4,11 @@ namespace FoundationaLLM.Common.Interfaces
 {
     public interface IGraphPrincipalWithGroups
     {
+        /// <summary>
+        /// Query group assignments for the provided principal (object) ID.
+        /// </summary>
+        /// <param name="principalId">The <see cref="string"/> containing the principal ID.</param>
+        /// <returns></returns>
         Task<List<Group>> GetGroups(string principalId);
     }
 }
--- src/dotnet/Common/Services/Security/Graph/GraphPrincipalWithGroupsBase.cs
+++ src/dotnet/Common/Services/Security/Graph/GraphPrincipalWithGroupsBase.cs
@@ -2,6 +2,10 @@
 
 namespace FoundationaLLM.Common.Services.Security.Graph
 {
+    /// <summary>
+    /// Abstract class for Microsoft Entra principals with queryable group assignments.
+    /// Refer to <see cref="ServicePrincipals"/> and <see cref="Users"/> classes.
+    /// </summary>
     public abstract class GraphPrincipalWithGroupsBase
     {
         protected readonly GraphServiceClient _graphServiceClient;
