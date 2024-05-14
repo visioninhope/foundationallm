f3adeb57f4f8d8edadc73d8cf4aba5c4a3fcf428
joelhulen <joelhulen@gmail.com>
2024-05-14 11:51:31 -0400

    Refactor and implement DI

--- src/dotnet/Common/Services/Security/Graph/GraphPrincipalWithGroupsBase.cs
+++ src/dotnet/Common/Services/Security/Graph/GraphPrincipalWithGroupsBase.cs
@@ -3,12 +3,18 @@
 namespace FoundationaLLM.Common.Services.Security.Graph
 {
     /// <summary>
-    /// Abstract class for Microsoft Entra principals with queryable group assignments.
-    /// Refer to <see cref="ServicePrincipals"/> and <see cref="Users"/> classes.
+    /// Abstract class for Microsoft Entra ID principals with queryable group assignments.
+    /// Refer to <see cref="ServicePrincipalsService"/> and <see cref="UsersService"/> classes.
     /// </summary>
-    public abstract class GraphPrincipalWithGroupsBase
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="GraphPrincipalWithGroupsBase"/> class.
+    /// </remarks>
+    /// <param name="graphServiceClient">The GraphServiceClient to be used for API interactions.</param>
+    public abstract class GraphPrincipalWithGroupsBase(GraphServiceClient graphServiceClient)
     {
-        protected readonly GraphServiceClient _graphServiceClient;
-        public GraphPrincipalWithGroupsBase(GraphServiceClient graphServiceClient) => _graphServiceClient = graphServiceClient;
+        /// <summary>
+        /// The GraphServiceClient used to interact with Microsoft Graph API.
+        /// </summary>
+        protected readonly GraphServiceClient _graphServiceClient = graphServiceClient;
     }
 }
