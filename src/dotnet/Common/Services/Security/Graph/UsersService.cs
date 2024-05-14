97b76567836c7d4a3f6a7c3225d1193dec483e6d
joelhulen <joelhulen@gmail.com>
2024-05-14 11:52:02 -0400

    Rename and implement DI

--- src/dotnet/Common/Services/Security/Graph/UsersService.cs
+++ src/dotnet/Common/Services/Security/Graph/UsersService.cs
@@ -4,9 +4,17 @@ using Microsoft.Graph.Models;
 
 namespace FoundationaLLM.Common.Services.Security.Graph
 {
-    public class Users : GraphPrincipalWithGroupsBase, IGraphPrincipalWithGroups
+
+    /// <summary>
+    /// Represents a service for querying the Microsoft Graph API for group membership of a user.
+    /// </summary>
+    public class UsersService : GraphPrincipalWithGroupsBase, IGraphPrincipalWithGroups
     {
-        public Users(GraphServiceClient graphServiceClient) : base(graphServiceClient) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UsersService"/> class.
+        /// </summary>
+        /// <param name="graphServiceClient">The Microsoft Graph API client.</param>
+        public UsersService(GraphServiceClient graphServiceClient) : base(graphServiceClient) { }
 
         /// <inheritdoc/>
         public async Task<List<Group>> GetGroups(string principalId)
@@ -20,10 +28,7 @@ namespace FoundationaLLM.Common.Services.Security.Graph
 
             while (groups?.Value != null)
             {
-                foreach (var group in groups.Value)
-                {
-                    groupMembership.Add(group);
-                }
+                groupMembership.AddRange(groups.Value);
 
                 // Invoke paging if required.
                 if (!string.IsNullOrEmpty(groups.OdataNextLink))
