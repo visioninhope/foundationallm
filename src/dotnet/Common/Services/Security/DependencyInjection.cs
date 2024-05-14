c8a5fad42801a5d9e6533b51a4dc6c7e56770270
joelhulen <joelhulen@gmail.com>
2024-05-14 11:52:32 -0400

    Configure DI for MS Graph Service and new graph services

--- src/dotnet/Common/Services/Security/DependencyInjection.cs
+++ src/dotnet/Common/Services/Security/DependencyInjection.cs
@@ -1,7 +1,11 @@
-﻿using FoundationaLLM.Common.Interfaces;
+﻿using Azure.Identity;
+using FoundationaLLM.Common.Authentication;
+using FoundationaLLM.Common.Interfaces;
 using FoundationaLLM.Common.Services.Security;
+using FoundationaLLM.Common.Services.Security.Graph;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Graph;
 
 namespace FoundationaLLM
 {
@@ -14,7 +18,15 @@ namespace FoundationaLLM
         /// Add group membership services to dependency injection container.
         /// </summary>
         /// <param name="builder">The host application builder.</param>
-        public static void AddGroupMembership(this IHostApplicationBuilder builder) =>
+        public static void AddGroupMembership(this IHostApplicationBuilder builder)
+        {
+            // Register the Microsoft Graph API client.
+            builder.Services.AddSingleton(provider => new GraphServiceClient(DefaultAuthentication.AzureCredential));
+
+            // Register graph and group membership services.
+            builder.Services.AddSingleton<IGraphPrincipalWithGroups, ServicePrincipalsService>();
+            builder.Services.AddSingleton<IGraphPrincipalWithGroups, UsersService>();
             builder.Services.AddScoped<IGroupMembershipService, MicrosoftGraphGroupMembershipService>();
+        }
     }
 }
