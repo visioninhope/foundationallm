d4801fc81792f8940c7e8e791d9eb4bc0f47704d
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2024-05-21 15:26:15 +0300

    Do not expand group membership for service principal identities

--- src/dotnet/Common/Services/Security/EntraUserClaimsProviderService.cs
+++ src/dotnet/Common/Services/Security/EntraUserClaimsProviderService.cs
@@ -27,6 +27,11 @@ namespace FoundationaLLM.Common.Services.Security
             };
         }
 
+        /// <inheritdoc/>
+        public bool IsServicePrincipal(ClaimsPrincipal userPrincipal) =>
+            // Service Principal tokens do not have a "scp" claim
+            userPrincipal.FindFirstValue(ClaimConstants.Scp) == null;
+
         /// <summary>
         /// Resolves the username from the provided <see cref="ClaimsPrincipal"/> object.
         /// </summary>
