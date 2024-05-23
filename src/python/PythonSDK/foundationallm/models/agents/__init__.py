358420f63ad222a08025b904e8cf5ecb706e5125
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2024-05-23 15:23:46 +0300

    Reorganize Python SDK to keep models.orchestration self-contained

--- src/python/PythonSDK/foundationallm/models/agents/__init__.py
+++ src/python/PythonSDK/foundationallm/models/agents/__init__.py
@@ -4,4 +4,3 @@ from .agent_vectorization_settings import AgentVectorizationSettings
 from .agent_base import AgentBase
 from .knowledge_management_agent import KnowledgeManagementAgent
 from .knowledge_management_completion_request import KnowledgeManagementCompletionRequest
-from .operation_types import OperationTypes
