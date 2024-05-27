1e0bff08952748eebebcf82ecd41900a2b6404ee
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2024-05-27 15:30:50 +0300

    Factor out object property parsing and transformation

--- src/python/PythonSDK/foundationallm/models/resource_providers/vectorization/indexing_profiles/azure_ai_search/azure_ai_search_indexing_profile.py
+++ src/python/PythonSDK/foundationallm/models/resource_providers/vectorization/indexing_profiles/azure_ai_search/azure_ai_search_indexing_profile.py
@@ -6,9 +6,12 @@ Classes:
 Description:
     Settings specific to the Azure AI Search Indexes
 """
+from typing import Any, Self
 from foundationallm.models.resource_providers.vectorization import IndexingProfileBase
 from .azure_ai_search_settings import AzureAISearchSettings
 from .azure_ai_search_configuration_references import AzureAISearchConfigurationReferences
+from foundationallm.models.utils import ObjectUtils
+from foundationallm.langchain.exceptions import LangChainException
 
 class AzureAISearchIndexingProfile(IndexingProfileBase):
     """
@@ -16,3 +19,18 @@ class AzureAISearchIndexingProfile(IndexingProfileBase):
     """
     settings: AzureAISearchSettings
     configuration_references: AzureAISearchConfigurationReferences
+
+    @staticmethod
+    def from_object(obj: Any) -> Self:
+
+        indexing_profile: AzureAISearchIndexingProfile = None
+        
+        try:
+            indexing_profile = AzureAISearchIndexingProfile(**ObjectUtils.translate_keys(obj))
+        except Exception as e:
+            raise LangChainException(f"The indexing profile object provided in the agent parameters is invalid. {str(e)}", 400)
+        
+        if indexing_profile is None:
+            raise LangChainException("The indexing object is missing in the agent parameters.", 400)
+
+        return indexing_profile
