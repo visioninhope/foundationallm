9f6ab10d40d3a6af40f1ccdac0f57c27638eefb6
kylebunting <kyle.bunting@gmail.com>
2024-05-29 11:32:02 -0600

    Updating KM agent to handle attachments

--- src/python/PythonSDK/foundationallm/langchain/agents/langchain_knowledge_management_agent.py
+++ src/python/PythonSDK/foundationallm/langchain/agents/langchain_knowledge_management_agent.py
@@ -1,5 +1,4 @@
-﻿from typing import List
-from langchain_community.callbacks import get_openai_callback
+﻿from langchain_community.callbacks import get_openai_callback
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
@@ -10,11 +9,89 @@ from foundationallm.models.orchestration import (
     CompletionResponse
 )
 from foundationallm.models.agents import KnowledgeManagementCompletionRequest
+from msclap import CLAP
+import requests
+from typing import List, Optional
+from foundationallm.storage import BlobStorageManager
+import uuid
+import os
+from pathlib import Path
 
 class LangChainKnowledgeManagementAgent(LangChainAgentBase):
     """
     The LangChain Knowledge Management agent.
-    """
+    """            
+    def get_prediction_from_audio_file(self, attachments: Optional[List[str]] = None) -> str:
+        """Generates embeddings from an audio file. Expects a file path as input. Outputs JSON containing the response from a REST API."""
+        if attachments is None:
+            return None
+
+        file = attachments[0].lstrip('/')
+        container_name = file.split('/')[0]
+        blob_path = file.replace(container_name, '').lstrip('/')
+        
+        # Load and initialize CLAP
+        clap_model = CLAP(version = '2023', use_cuda=True)
+        
+        try:
+            storage_manager = BlobStorageManager(
+                account_name=self.config.get_value('FoundationaLLM:Attachment:ResourceProviderService:Storage:AccountName'),
+                container_name=file.split('/')[0],
+                authentication_type=self.config.get_value('FoundationaLLM:Attachment:ResourceProviderService:Storage:AuthenticationType')
+            )
+        except Exception as e:
+            raise e
+
+        try:
+            # Load the file from storage.
+            if (storage_manager.file_exists(blob_path) == False):
+                raise FileNotFoundError(f'The file {blob_path} was not found in blob storage.')
+            blob = storage_manager.read_file_content(blob_path)
+        except Exception as e:
+            raise e
+
+        output_dir = "/classify_tmp/audio_data"
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
+        
+        local_file_path = output_dir + str(uuid.uuid4()) + ".wav"
+        try:
+            # Save the file locally, so it can be reference by CLAP
+            with open(local_file_path, "wb") as local_file:
+                local_file.write(bytearray(blob))
+        except Exception as e:
+            raise e
+
+        file_paths = [local_file_path]
+        
+        audio_embeddings = clap_model.get_audio_embeddings(file_paths, resample=True)
+        data = audio_embeddings.numpy().tolist()
+
+        base_url = self.config.get_value('FoundationaLLM:APIs:AudioClassificationAPI:APIUrl').rstrip('/')
+        endpoint = self.config.get_value('FoundationaLLM:APIs:AudioClassificationAPI:Classification:PredictionEndpoint')
+        #base_url = 'http://localhost:8865'.rstrip('/')
+        #endpoint = '/classification/predict'
+        api_endpoint = f'{base_url}{endpoint}'
+        print(api_endpoint)
+
+        # Create the embeddings payload.
+        payload = {"embeddings": data}
+        # TODO: Need to add in correct headers, including any auth headers.
+        headers = {"charset": "utf-8", "Content-Type": "application/json"}
+        # Make the REST API call.
+        r = requests.post(api_endpoint, json=payload, headers=headers)
+        # Check the response status code.
+        if r.status_code != 200:
+            raise Exception(f'Error: ({r.status_code}) {r.text}')
+        # Return the JSON response.
+        response = r.json()
+
+        # Clean up the local file
+        try:
+            os.remove(local_file_path)
+        except Exception as e:
+            raise e
+     
+        return response['label']
     
     def invoke(self, request: KnowledgeManagementCompletionRequest) -> CompletionResponse:
         """
@@ -51,8 +128,12 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
                         request.message_history,
                         conversation_history.max_history)
 
-                # Insert the context into the template.
-                prompt_builder += '{context}'   
+                classification_prediction = self.get_prediction_from_audio_file(request.attachments)
+                if classification_prediction is not None:
+                    prompt_builder += f"\n\CONTEXT: {classification_prediction}"
+                else:
+                    # Insert the context into the template.
+                    prompt_builder += '{context}'
 
                 # Add the suffix, if it exists.
                 if prompt.suffix is not None:
@@ -89,13 +170,15 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
                 else:
                     chain_context = { "context": RunnablePassthrough() }
 
+                output_parser = StrOutputParser()
+
                 # Compose LCEL chain
                 chain = (
                     chain_context
                     | prompt_template
                     | RunnableLambda(self._record_full_prompt)
                     | self._get_language_model(agent.orchestration_settings, request.settings)
-                    | StrOutputParser()
+                    | output_parser
                 )
 
                 completion = chain.invoke(request.user_prompt)
