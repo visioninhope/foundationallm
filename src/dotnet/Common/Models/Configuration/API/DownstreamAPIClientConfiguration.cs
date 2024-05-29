ac4e36517a9d35bdca477e72b5a15190a928f486
joelhulen <joelhulen@gmail.com>
2024-05-29 15:59:19 -0400

    Update the API client configuration model to include timeouts

--- src/dotnet/Common/Models/Configuration/API/DownstreamAPIClientConfiguration.cs
+++ src/dotnet/Common/Models/Configuration/API/DownstreamAPIClientConfiguration.cs
@@ -5,19 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using FoundationaLLM.Common.Interfaces;
 
-namespace FoundationaLLM.Common.Authentication
+namespace FoundationaLLM.Common.Models.Configuration.API
 {
     /// <inheritdoc/>
     public record DownstreamAPISettings : IDownstreamAPISettings
     {
         /// <inheritdoc/>
-        public required Dictionary<string, DownstreamAPIKeySettings> DownstreamAPIs { get; init; }
+        public required Dictionary<string, DownstreamAPIClientConfiguration> DownstreamAPIs { get; init; }
     }
 
     /// <summary>
-    /// Represents settings for downstream API key authentication.
+    /// Represents settings for downstream API clients.
     /// </summary>
-    public record DownstreamAPIKeySettings
+    public record DownstreamAPIClientConfiguration
     {
         /// <summary>
         /// The URL of the downstream API.
@@ -27,5 +27,11 @@ namespace FoundationaLLM.Common.Authentication
         /// The value of the API key.
         /// </summary>
         public required string APIKey { get; init; }
+        /// <summary>
+        /// Specifies the timeout for the downstream API HTTP client.
+        /// If this value is null, the default timeout is used.
+        /// For an infinite waiting period, use <see cref="Timeout.InfiniteTimeSpan"/>
+        /// </summary>
+        public TimeSpan? Timeout { get; init; }
     }
 }
