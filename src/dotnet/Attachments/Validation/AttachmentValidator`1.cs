75b1445fdb25a50d59ee0f381d3a51949a270309
joelhulen <joelhulen@gmail.com>
2024-05-29 19:41:22 -0400

    Rename AttachmentBase to AttachmentFile

--- src/dotnet/Attachments/Validation/AttachmentValidator`1.cs
+++ src/dotnet/Attachments/Validation/AttachmentValidator`1.cs
@@ -8,14 +8,14 @@ namespace FoundationaLLM.Attachment.Validation
     /// Base validator for attachments.
     /// </summary>
     /// <typeparam name="T">The type of attachment to validate.</typeparam>
-    public class AttachmentValidator<T> : AbstractValidator<T> where T : AttachmentBase
+    public class AttachmentValidator<T> : AbstractValidator<T> where T : AttachmentFile
     {
         /// <summary>
         /// Validates the value of a specified configuration reference.
         /// </summary>
         /// <param name="attachment">The attachment object being validated.</param>
         /// <returns>True if the value of the configuration reference is valid, False otherwise.</returns>
-        protected bool ValidConfigurationReference(AttachmentBase attachment) =>
+        protected bool ValidConfigurationReference(AttachmentFile attachment) =>
             true;
     }
 }
--- src/dotnet/CoreAPI/Program.cs
+++ src/dotnet/CoreAPI/Program.cs
@@ -92,9 +92,6 @@ namespace FoundationaLLM.Core.API
             builder.AddAgentResourceProvider();
             builder.AddAttachmentResourceProvider();
 
-            // Activate all resource providers (give them a chance to initialize).
-            builder.Services.ActivateSingleton<IEnumerable<IResourceProviderService>>();
-
             // Register the downstream services and HTTP clients.
             RegisterDownstreamServices(builder);
 
