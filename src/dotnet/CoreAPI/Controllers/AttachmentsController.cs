2ef6b56f8a160dcdde8aa1e10f6da1df7b28661c
joelhulen <joelhulen@gmail.com>
2024-05-28 23:51:52 -0400

    Initial Attachments controller

---
+++ src/dotnet/CoreAPI/Controllers/AttachmentsController.cs
@@ -0,0 +1,44 @@
+﻿using Asp.Versioning;
+using FoundationaLLM.Common.Models.Configuration.Branding;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace FoundationaLLM.Core.API.Controllers
+{
+    /// <summary>
+    /// Provides branding information for the client.
+    /// </summary>
+    [Authorize(Policy = "DefaultPolicy")]
+    [ApiController]
+    [Route("[controller]")]
+    public class AttachmentsController(ILogger<AttachmentsController> logger) : ControllerBase
+    {
+        /// <summary>
+        /// Retrieves the uploaded attachments.
+        /// </summary>
+        [HttpGet(Name = "Get")]
+        public IActionResult Index() =>
+            Ok();
+
+        /// <summary>
+        /// Uploads an attachment.
+        /// </summary>
+        /// <param name="file">The file sent with the HTTP request.</param>
+        /// <returns></returns>
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("File not selected.");
+
+            var fileName = file.FileName;
+            var contentType = file.ContentType;
+
+            //using (var stream = file.OpenReadStream())
+            //{
+            //}
+            return Ok();
+        }
+    }
+}
