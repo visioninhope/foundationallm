4e42f4994045a8dc150217812e79809cabe75b29
joelhulen <joelhulen@gmail.com>
2024-06-05 00:10:36 -0400

    Add models to support user and group account retrieval

---
+++ src/dotnet/Common/Models/Authentication/AccountQueryParameters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Common.Models.Authentication
+{
+    /// <summary>
+    /// Parameters for querying user and group accounts.
+    /// </summary>
+    public class AccountQueryParameters
+    {
+        /// <summary>
+        /// Account name.
+        /// </summary>
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// The current page number.
+        /// </summary>
+        [JsonPropertyName("page_number")]
+        public int? PageNumber { get; set; }
+
+        /// <summary>
+        /// The number of items to return in each page.
+        /// </summary>
+        [JsonPropertyName("page_size")]
+        public int? PageSize { get; set; }
+    }
+}
---
+++ src/dotnet/Common/Models/Authentication/UserAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Common.Models.Authentication
+{
+    /// <summary>
+    /// Stores user account information.
+    /// </summary>
+    public class UserAccount
+    {
+        /// <summary>
+        /// User account identifier.
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// User account name.
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// User account email address.
+        /// </summary>
+        [JsonPropertyName("email")]
+        public string? Email { get; set; }
+    }
+}
