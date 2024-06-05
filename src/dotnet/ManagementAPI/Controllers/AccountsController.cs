c6b51baf9d78a52aa663960149adcab559010157
joelhulen <joelhulen@gmail.com>
2024-06-05 07:43:35 -0400

    Add user methods

--- src/dotnet/ManagementAPI/Controllers/AccountsController.cs
+++ src/dotnet/ManagementAPI/Controllers/AccountsController.cs
@@ -10,7 +10,7 @@ namespace FoundationaLLM.Management.API.Controllers
     /// Provides account retrieval methods.
     /// </summary>
     /// <param name="callContext">The call context containing user identity details.</param>
-    /// <param name="groupService">The <see cref="IGroupService"/> used for retrieving group account information.</param>
+    /// <param name="accountService">The <see cref="IAccountService"/> used for retrieving group account information.</param>
     /// <param name="logger">The <see cref="ILogger"/> used for logging.</param>
     [Authorize(Policy = "DefaultPolicy")]
     [ApiController]
@@ -19,7 +19,7 @@ namespace FoundationaLLM.Management.API.Controllers
     [Route($"instances/{{instanceId}}/accounts")]
     public class AccountsController(
         ICallContext callContext,
-        IGroupService groupService,
+        IAccountService accountService,
         ILogger<AccountsController> logger) : Controller
     {
         private readonly ILogger<AccountsController> _logger = logger;
@@ -32,7 +32,7 @@ namespace FoundationaLLM.Management.API.Controllers
         [HttpGet("groups", Name = "GetGroups")]
         public async Task<IActionResult> GetGroups(AccountQueryParameters parameters)
         {
-            var groups = await groupService.GetUserGroupsAsync(parameters);
+            var groups = await accountService.GetUserGroupsAsync(parameters);
             return new OkObjectResult(groups);
         }
 
@@ -44,8 +44,31 @@ namespace FoundationaLLM.Management.API.Controllers
         [HttpGet("groups/{groupId}", Name = "GetGroup")]
         public async Task<IActionResult> GetGroup(string groupId)
         {
-            var group = await groupService.GetUserGroupByIdAsync(groupId);
+            var group = await accountService.GetUserGroupByIdAsync(groupId);
             return new OkObjectResult(group);
         }
-}
+
+        /// <summary>
+        /// Retrieves a list of user accounts with filtering and paging options.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("users", Name = "GetUsers")]
+        public async Task<IActionResult> GetUsers(AccountQueryParameters parameters)
+        {
+            var users = await accountService.GetUsersAsync(parameters);
+            return new OkObjectResult(users);
+        }
+
+        /// <summary>
+        /// Retrieves a specific user account by its identifier.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpGet("users/{userId}", Name = "GetUser")]
+        public async Task<IActionResult> GetUser(string userId)
+        {
+            var user = await accountService.GetUserByIdAsync(userId);
+            return new OkObjectResult(user);
+        }
+    }
 }
