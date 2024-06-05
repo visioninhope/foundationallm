42fda95056b0a845aa12234b7cde15469359c91c
joelhulen <joelhulen@gmail.com>
2024-06-05 00:07:56 -0400

    Rename group service and add group retrieval and filtering functionality

---
+++ src/dotnet/Common/Interfaces/IGroupService.cs
@@ -0,0 +1,32 @@
+﻿using FoundationaLLM.Common.Models.Authentication;
+using FoundationaLLM.Common.Models.Collections;
+
+namespace FoundationaLLM.Common.Interfaces
+{
+    /// <summary>
+    /// Provides group membership services.
+    /// </summary>
+    public interface IGroupService
+    {
+        /// <summary>
+        /// Retrieves the group identifiers list of the groups where the specified user principal is a member.
+        /// </summary>
+        /// <param name="userIdentifier">The user identifier for which group membership is retrieved. Can be either an object id or a user principal name (UPN).</param>
+        /// <returns></returns>
+        Task<List<string>> GetGroupsForPrincipalAsync(string userIdentifier);
+
+        /// <summary>
+        /// Retrieves a list of group accounts with filtering and paging options.
+        /// </summary>
+        /// <param name="queryParams">The filtering and paging options used when retrieving group accounts.</param>
+        /// <returns></returns>
+        Task<PagedResponse<GroupAccount>> GetUserGroupsAsync(AccountQueryParameters queryParams);
+
+        /// <summary>
+        /// Retrieves a group account by its identifier.
+        /// </summary>
+        /// <param name="groupId">The group account identifier used to retrieve a single group account.</param>
+        /// <returns></returns>
+        Task<GroupAccount> GetUserGroupByIdAsync(string groupId);
+    }
+}
---
+++ src/dotnet/Common/Services/Security/MicrosoftGraphGroupService.cs
@@ -0,0 +1,125 @@
+﻿using Azure.Search.Documents.Models;
+using FoundationaLLM.Common.Interfaces;
+using FoundationaLLM.Common.Models.Authentication;
+using FoundationaLLM.Common.Models.Collections;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Microsoft.Graph.Groups;
+using Microsoft.Graph.Models;
+using Microsoft.Kiota.Abstractions;
+
+namespace FoundationaLLM.Common.Services.Security
+{
+    /// <summary>
+    /// Implements group membership services using the Microsoft Graph API.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="MicrosoftGraphGroupService"/> class.
+    /// </remarks>
+    /// <param name="graphServiceClient">The GraphServiceClient to be used for API interactions.</param>
+    /// <param name="logger">The logger used for logging.</param>
+    public class MicrosoftGraphGroupService(
+        GraphServiceClient graphServiceClient,
+        ILogger<MicrosoftGraphGroupService> logger) : IGroupService
+    {
+        private readonly ILogger<MicrosoftGraphGroupService> _logger = logger;
+
+        /// <inheritdoc/>
+        public async Task<List<string>> GetGroupsForPrincipalAsync(string userIdentifier)
+        {
+            var groups = await graphServiceClient.Users[userIdentifier].TransitiveMemberOf.GraphGroup.GetAsync(requestConfiguration =>
+            {
+                requestConfiguration.QueryParameters.Top = 500;
+            }).ConfigureAwait(false);
+
+            var groupMembership = new List<Group>();
+
+            while (groups?.Value != null)
+            {
+                groupMembership.AddRange(groups.Value);
+
+                // Invoke paging if required.
+                if (!string.IsNullOrEmpty(groups.OdataNextLink))
+                {
+                    groups = await graphServiceClient.Users[userIdentifier].TransitiveMemberOf.GraphGroup
+                        .WithUrl(groups.OdataNextLink)
+                        .GetAsync();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return groupMembership.Count == 0
+                ? []
+                : groupMembership.Where(x => x.Id != null).Select(x => x.Id!).ToList();
+        }
+
+        /// <inheritdoc/>
+        public async Task<GroupAccount> GetUserGroupByIdAsync(string groupId)
+        {
+            var group = await graphServiceClient.Groups[groupId].GetAsync();
+
+            return new GroupAccount
+            {
+                Id = group?.Id,
+                Name = group?.DisplayName
+            };
+        }
+        /// <inheritdoc/>
+        public async Task<PagedResponse<GroupAccount>> GetUserGroupsAsync(AccountQueryParameters queryParams)
+        {
+            // TODO: Authorize the user to access this endpoint via the AuthorizationService.
+            // A user must be assigned the "Role Based Access Control Administrator" role (/providers/FoundationaLLM.Authorization/roleDefinitions/17ca4b59-3aee-497d-b43b-95dd7d916f99) to access this endpoint.
+
+            var pageSize = queryParams.PageSize ?? 100;
+            var userGroups = new List<GroupAccount>();
+
+            var currentPage = 1;
+
+            // Retrieve group accounts with filtering and paging options.
+            var groupsPage = await graphServiceClient.Groups
+                .GetAsync(requestConfiguration =>
+                {
+                    requestConfiguration.QueryParameters.Select = ["id", "displayName"];
+                    requestConfiguration.QueryParameters.Filter = "securityEnabled eq true";
+                    if (!string.IsNullOrEmpty(queryParams.Name))
+                    {
+                        requestConfiguration.QueryParameters.Search = $"\"displayName:{queryParams.Name}\"";
+                    }
+                    requestConfiguration.QueryParameters.Orderby = ["displayName"];
+                    requestConfiguration.QueryParameters.Top = pageSize;
+                    requestConfiguration.QueryParameters.Count = true;
+                    requestConfiguration.Headers.Add("ConsistencyLevel", "eventual");
+                });
+
+            // Skip pages until we reach the desired page.
+            while (groupsPage?.OdataNextLink != null && currentPage < queryParams.PageNumber)
+            {
+                groupsPage = await graphServiceClient.Groups
+                    .WithUrl(groupsPage.OdataNextLink)
+                    .GetAsync();
+                currentPage++;
+            }
+
+            // Process the desired page.
+            if (groupsPage?.Value != null)
+            {
+                userGroups.AddRange(groupsPage.Value.Select(x => new GroupAccount
+                {
+                    Id = x.Id,
+                    Name = x.DisplayName
+                }));
+            }
+
+            return new PagedResponse<GroupAccount>
+            {
+                Items = userGroups,
+                TotalItems = groupsPage?.OdataCount,
+                HasNextPage = groupsPage?.OdataNextLink != null
+            };
+        }
+    }
+}
