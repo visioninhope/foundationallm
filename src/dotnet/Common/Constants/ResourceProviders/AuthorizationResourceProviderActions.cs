89fd7a8851ed1740cda23d3896ff47116d22275f
Andrei Alistar <andrei.alistar@genisoft.eu>
2024-06-07 10:57:56 +0300

    added missing xml comments

--- src/dotnet/Common/Constants/ResourceProviders/AuthorizationResourceProviderActions.cs
+++ src/dotnet/Common/Constants/ResourceProviders/AuthorizationResourceProviderActions.cs
@@ -5,8 +5,17 @@
     /// </summary>
     public static class AuthorizationResourceProviderActions
     {
+        /// <summary>
+        /// Get user accounts.
+        /// </summary>
         public const string GetUsers = "getUsers";
+        /// <summary>
+        /// Get security groups.
+        /// </summary>
         public const string GetGroups = "getGroups";
+        /// <summary>
+        /// Get account objects (users and groups).
+        /// </summary>
         public const string GetObjects = "getObjects";
     }
 }
