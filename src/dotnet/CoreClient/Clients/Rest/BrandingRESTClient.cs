30d7728c0e9c71628fb94890a0fca9b841e92591
joelhulen <joelhulen@gmail.com>
2024-06-11 15:29:09 -0400

    Create the branding REST client

---
+++ src/dotnet/CoreClient/Clients/Rest/BrandingRESTClient.cs
@@ -0,0 +1,33 @@
+﻿using FoundationaLLM.Client.Core.Interfaces;
+using FoundationaLLM.Common.Models.Configuration.Branding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Client.Core.Clients.Rest
+{
+    /// <summary>
+    /// Provides methods to manage calls to the Core API's Branding endpoints.
+    /// </summary>
+    public class BrandingRESTClient(IHttpClientFactory httpClientFactory) : CoreRESTClientBase(httpClientFactory), IBrandingRESTClient
+    {
+        /// <inheritdoc/>
+        public async Task<ClientBrandingConfiguration> GetBrandingAsync(string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.GetAsync("branding");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var branding = JsonSerializer.Deserialize<ClientBrandingConfiguration>(responseContent, SerializerOptions);
+                return branding ?? throw new InvalidOperationException("The returned branding information is invalid.");
+            }
+
+            throw new Exception($"Failed to retrieve branding information. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+    }
+}
---
+++ src/dotnet/CoreClient/Interfaces/IBrandingRESTClient.cs
@@ -0,0 +1,17 @@
+﻿using FoundationaLLM.Common.Models.Configuration.Branding;
+
+namespace FoundationaLLM.Client.Core.Interfaces
+{
+    /// <summary>
+    /// Provides methods to manage calls to the Core API's Branding endpoints.
+    /// </summary>
+    public interface IBrandingRESTClient
+    {
+        /// <summary>
+        /// Retrieves the branding information for the client.
+        /// </summary>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task<ClientBrandingConfiguration> GetBrandingAsync(string token);
+    }
+}
