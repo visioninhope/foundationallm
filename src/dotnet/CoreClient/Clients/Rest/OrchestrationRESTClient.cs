0e9f2843f75cb489f61fdfc8707ef475af6fec5a
joelhulen <joelhulen@gmail.com>
2024-06-11 15:29:51 -0400

    Create the orchestration REST client

---
+++ src/dotnet/CoreClient/Clients/Rest/OrchestrationRESTClient.cs
@@ -0,0 +1,58 @@
+﻿using FoundationaLLM.Common.Models.Chat;
+using FoundationaLLM.Common.Models.Orchestration;
+using FoundationaLLM.Common.Models.ResourceProviders.Agent;
+using FoundationaLLM.Common.Models.ResourceProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FoundationaLLM.Client.Core.Interfaces;
+
+namespace FoundationaLLM.Client.Core.Clients.Rest
+{
+    /// <summary>
+    /// Provides methods to manage calls to the Core API's orchestration endpoints.
+    /// </summary>
+    public class OrchestrationRESTClient(IHttpClientFactory httpClientFactory) : CoreRESTClientBase(httpClientFactory), IOrchestrationRESTClient
+    {
+        /// <inheritdoc/>
+        public async Task<Completion> SendOrchestrationCompletionRequestAsync(CompletionRequest completionRequest, string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var serializedRequest = JsonSerializer.Serialize(completionRequest, SerializerOptions);
+
+            var responseMessage = await coreClient.PostAsync("orchestration/completion", // Session-less - no message history or data retention in Cosmos DB.
+                new StringContent(
+                    serializedRequest,
+                    Encoding.UTF8, "application/json"));
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var completionResponse =
+                    JsonSerializer.Deserialize<Completion>(responseContent, SerializerOptions);
+                return completionResponse ?? throw new InvalidOperationException("The returned completion response is invalid.");
+            }
+
+            throw new Exception($"Failed to send completion request. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<ResourceProviderGetResult<AgentBase>>> GetAgentsAsync(string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.GetAsync("orchestration/agents");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var agents = JsonSerializer.Deserialize<IEnumerable<ResourceProviderGetResult<AgentBase>>>(responseContent, SerializerOptions);
+                return agents ?? throw new InvalidOperationException("The returned agents are invalid.");
+            }
+
+            throw new Exception($"Failed to retrieve agents. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+    }
+}
---
+++ src/dotnet/CoreClient/Interfaces/IOrchestrationRESTClient.cs
@@ -0,0 +1,28 @@
+﻿using FoundationaLLM.Common.Models.Chat;
+using FoundationaLLM.Common.Models.Orchestration;
+using FoundationaLLM.Common.Models.ResourceProviders;
+using FoundationaLLM.Common.Models.ResourceProviders.Agent;
+
+namespace FoundationaLLM.Client.Core.Interfaces
+{
+    /// <summary>
+    /// Provides methods to manage calls to the Core API's orchestration endpoints.
+    /// </summary>
+    public interface IOrchestrationRESTClient
+    {
+        /// <summary>
+        /// Performs a sessionless request to the Core API.
+        /// </summary>
+        /// <param name="completionRequest">The completion request data sent to the endpoint.</param>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task<Completion> SendOrchestrationCompletionRequestAsync(CompletionRequest completionRequest, string token);
+
+        /// <summary>
+        /// Retrieves agents available to the user for orchestration and session-based requests.
+        /// </summary>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task<IEnumerable<ResourceProviderGetResult<AgentBase>>> GetAgentsAsync(string token);
+    }
+}
