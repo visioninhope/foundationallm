d4d1336308068f2be7d620b4e5f4fba3e04cb1d0
joelhulen <joelhulen@gmail.com>
2024-06-11 15:30:34 -0400

    Create the status REST client

---
+++ src/dotnet/CoreClient/Clients/Rest/StatusRESTClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoundationaLLM.Client.Core.Interfaces;
+
+namespace FoundationaLLM.Client.Core.Clients.Rest
+{
+    public class StatusRESTClient(IHttpClientFactory httpClientFactory) : CoreRESTClientBase(httpClientFactory), IStatusRESTClient
+    {
+        /// <inheritdoc/>
+        public async Task<string> GetServiceStatusAsync(string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.GetAsync("status");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return "Service is up and running.";
+            }
+
+            throw new Exception($"Failed to retrieve service status. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task<string> GetAuthStatusAsync(string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.GetAsync("status/auth");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return "Authentication is successful.";
+            }
+
+            throw new Exception($"Failed to retrieve authentication status. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+    }
+}
---
+++ src/dotnet/CoreClient/Interfaces/IStatusRESTClient.cs
@@ -0,0 +1,22 @@
+﻿namespace FoundationaLLM.Client.Core.Interfaces
+{
+    /// <summary>
+    /// Provides methods to manage calls to the Core API's status endpoints.
+    /// </summary>
+    public interface IStatusRESTClient
+    {
+        /// <summary>
+        /// Returns the status of the Core API service.
+        /// </summary>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task<string> GetServiceStatusAsync(string token);
+
+        /// <summary>
+        /// Returns OK if the requester is authenticated and allowed to execute
+        /// requests against the Core API service.
+        /// </summary>
+        /// <param name="token">The authentication token to send with the request.</param>
+        Task<string> GetAuthStatusAsync(string token);
+    }
+}
