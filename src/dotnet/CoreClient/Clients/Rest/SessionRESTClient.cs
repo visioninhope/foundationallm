bc67c4072e377b29808bbdc9d433d7717298f066
joelhulen <joelhulen@gmail.com>
2024-06-11 15:30:03 -0400

    Create the session REST client

---
+++ src/dotnet/CoreClient/Clients/Rest/SessionRESTClient.cs
@@ -0,0 +1,160 @@
+﻿using FoundationaLLM.Client.Core.Interfaces;
+using FoundationaLLM.Common.Models.Chat;
+using FoundationaLLM.Common.Models.Orchestration;
+using FoundationaLLM.Common.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Client.Core.Clients.Rest
+{
+    /// <summary>
+    /// Provides methods to manage calls to the Core API's session endpoints.
+    /// </summary>
+    public class SessionRESTClient(IHttpClientFactory httpClientFactory) : CoreRESTClientBase(httpClientFactory), ISessionRESTClient
+    {
+        /// <inheritdoc/>
+        public async Task<string> CreateSessionAsync(string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseSession = await coreClient.PostAsync("sessions", null);
+
+            if (responseSession.IsSuccessStatusCode)
+            {
+                var responseContent = await responseSession.Content.ReadAsStringAsync();
+                var sessionResponse = JsonSerializer.Deserialize<Session>(responseContent, SerializerOptions);
+                if (sessionResponse?.SessionId != null)
+                {
+                    return sessionResponse.SessionId;
+                }
+            }
+
+            throw new Exception($"Failed to create a new chat session. Status code: {responseSession.StatusCode}. Reason: {responseSession.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task<string> RenameChatSession(string sessionId, string sessionName, string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var response = await coreClient.PostAsync($"sessions/{sessionId}/rename?newChatSessionName={UrlEncoder.Default.Encode(sessionName)}", null);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return sessionName;
+            }
+
+            throw new Exception($"Failed to rename chat session. Status code: {response.StatusCode}. Reason: {response.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task<Completion> SendSessionCompletionRequestAsync(OrchestrationRequest orchestrationRequest, string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var serializedRequest = JsonSerializer.Serialize(orchestrationRequest, SerializerOptions);
+
+            var sessionUrl = $"sessions/{orchestrationRequest.SessionId}/completion"; // Session-based - message history and data is retained in Cosmos DB. Must create a session if it does not exist.
+            var responseMessage = await coreClient.PostAsync(sessionUrl,
+                new StringContent(
+                    serializedRequest,
+                    Encoding.UTF8, "application/json"));
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var completionResponse =
+                    JsonSerializer.Deserialize<Completion>(responseContent, SerializerOptions);
+                return completionResponse ?? throw new InvalidOperationException("The returned completion response is invalid.");
+            }
+
+            throw new Exception($"Failed to send completion request. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task<CompletionPrompt> GetCompletionPromptAsync(string sessionId, string completionPromptId, string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.GetAsync($"sessions/{sessionId}/completionprompts/{completionPromptId}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var completionPrompt =
+                    JsonSerializer.Deserialize<CompletionPrompt>(responseContent, SerializerOptions);
+                return completionPrompt ?? throw new InvalidOperationException("The returned completion prompt is invalid.");
+            }
+
+            throw new Exception($"Failed to get completion prompt. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<Message>> GetChatSessionMessagesAsync(string sessionId, string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.GetAsync($"sessions/{sessionId}/messages");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var messages = JsonSerializer.Deserialize<IEnumerable<Message>>(responseContent, SerializerOptions);
+                return messages ?? throw new InvalidOperationException("The returned messages are invalid.");
+            }
+
+            throw new Exception($"Failed to get chat session messages. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<Session>> GetAllChatSessionsAsync(string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.GetAsync("sessions");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var sessions = JsonSerializer.Deserialize<IEnumerable<Session>>(responseContent, SerializerOptions);
+                return sessions ?? throw new InvalidOperationException("The returned sessions are invalid.");
+            }
+
+            throw new Exception($"Failed to retrieve chat sessions. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task RateMessageAsync(string sessionId, string messageId, bool rating, string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.PostAsync($"sessions/{sessionId}/message/{messageId}/rate?rating={rating}", null);
+
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to rate message. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task<string> SummarizeChatSessionNameAsync(string sessionId, string prompt, string token)
+        {
+            var coreClient = GetCoreClient(token);
+            var responseMessage = await coreClient.PostAsync($"sessions/{sessionId}/summarize-name", new StringContent(JsonSerializer.Serialize(prompt), Encoding.UTF8, "application/json"));
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var completion = JsonSerializer.Deserialize<Completion>(responseContent, SerializerOptions);
+                return completion?.Text ?? throw new InvalidOperationException("The returned summary is invalid.");
+            }
+
+            throw new Exception($"Failed to summarize chat session name. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task DeleteSessionAsync(string sessionId, string token)
+        {
+            var coreClient = GetCoreClient(token);
+            await coreClient.DeleteAsync($"sessions/{sessionId}");
+        }
+    }
+}
---
+++ src/dotnet/CoreClient/Interfaces/ISessionRESTClient.cs
@@ -0,0 +1,81 @@
+﻿using FoundationaLLM.Common.Models.Chat;
+using FoundationaLLM.Common.Models.Orchestration;
+
+namespace FoundationaLLM.Client.Core.Interfaces
+{
+    /// <summary>
+    /// Provides methods to manage calls to the Core API's session endpoints.
+    /// </summary>
+    public interface ISessionRESTClient
+    {
+        /// <summary>
+        /// Retrieves all chat sessions.
+        /// </summary>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task<IEnumerable<Session>> GetAllChatSessionsAsync(string token);
+
+        /// <summary>
+        /// Sets the rating for a message.
+        /// </summary>
+        /// <param name="sessionId">The chat session ID that contains the message to rate.</param>
+        /// <param name="messageId">The ID of the message to rate.</param>
+        /// <param name="rating">Set to true for a positive rating and false for a negative rating.</param>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task RateMessageAsync(string sessionId, string messageId, bool rating, string token);
+
+        /// <inheritdoc/>
+        Task<string> SummarizeChatSessionNameAsync(string sessionId, string prompt, string token);
+
+        /// <summary>
+        /// Creates and renames a session.
+        /// </summary>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns>Returns the new Session ID.</returns>
+        Task<string> CreateSessionAsync(string token);
+
+        /// <summary>
+        /// Renames a chat session.
+        /// </summary>
+        /// <param name="sessionId">The chat session ID.</param>
+        /// <param name="sessionName">The new session name.</param>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task<string> RenameChatSession(string sessionId, string sessionName, string token);
+
+        /// <summary>
+        /// Sends a user prompt to the specified agent within the specified session.
+        /// </summary>
+        /// <param name="orchestrationRequest"></param>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns>Returns a completion response.</returns>
+        /// <exception cref="FoundationaLLMException"></exception>
+        Task<Completion> SendSessionCompletionRequestAsync(OrchestrationRequest orchestrationRequest, string token);
+
+        /// <summary>
+        /// Gets a completion prompt by session ID and completion prompt ID.
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <param name="completionPromptId"></param>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task<CompletionPrompt> GetCompletionPromptAsync(string sessionId, string completionPromptId, string token);
+
+        /// <summary>
+        /// Returns the chat messages related to an existing session.
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task<IEnumerable<Message>> GetChatSessionMessagesAsync(string sessionId, string token);
+
+        /// <summary>
+        /// Deletes a chat session.
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <param name="token">The authentication token to send with the request.</param>
+        /// <returns></returns>
+        Task DeleteSessionAsync(string sessionId, string token);
+    }
+}
