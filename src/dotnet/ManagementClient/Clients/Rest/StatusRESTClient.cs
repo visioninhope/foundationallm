21b24cfec7c57cd7deaffb6f8e614d2267c9213c
joelhulen <joelhulen@gmail.com>
2024-06-22 09:05:37 -0400

    Add Status REST client

---
+++ src/dotnet/ManagementClient/Clients/Rest/StatusRESTClient.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Core;
+using FoundationaLLM.Client.Management.Interfaces;
+
+namespace FoundationaLLM.Client.Management.Clients.Rest
+{
+    internal class StatusRESTClient(
+        IHttpClientFactory httpClientFactory,
+        TokenCredential credential) : ManagementRESTClientBase(httpClientFactory, credential), IStatusRESTClient
+    {
+        /// <inheritdoc/>
+        public async Task<string> GetServiceStatusAsync()
+        {
+            var coreClient = await GetManagementClientAsync();
+            var responseMessage = await coreClient.GetAsync("status");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return "Service is up and running.";
+            }
+
+            throw new Exception($"Failed to retrieve service status. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+
+        /// <inheritdoc/>
+        public async Task<string> GetAuthStatusAsync()
+        {
+            var coreClient = await GetManagementClientAsync();
+            var responseMessage = await coreClient.GetAsync("status/auth");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return "Authentication is successful.";
+            }
+
+            throw new Exception($"Failed to retrieve authentication status. Status code: {responseMessage.StatusCode}. Reason: {responseMessage.ReasonPhrase}");
+        }
+    }
+}
