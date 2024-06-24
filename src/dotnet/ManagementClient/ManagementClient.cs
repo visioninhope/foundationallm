233db0b3a88a13da6a406dce5876292b95b03a50
joelhulen <joelhulen@gmail.com>
2024-06-24 19:49:42 -0400

    Add attachments client

--- src/dotnet/ManagementClient/ManagementClient.cs
+++ src/dotnet/ManagementClient/ManagementClient.cs
@@ -58,12 +58,14 @@ namespace FoundationaLLM.Client.Management
         }
 
         public IAgentManagementClient Agents { get; private set; } = null!;
-        public IPromptManagementClient Prompts { get; private set; } = null!;
+        public IAttachmentManagementClient Attachments { get; private set; } = null!;
         public IDataSourceManagementClient DataSources { get; private set; } = null!;
+        public IPromptManagementClient Prompts { get; private set; } = null!;
 
         private void InitializeClients()
         {
             Agents = new AgentManagementClient(_managementRestClient);
+            Attachments = new AttachmentManagementClient(_managementRestClient);
             Prompts = new PromptManagementClient(_managementRestClient);
             DataSources = new DataSourceManagementClient(_managementRestClient);
         }
