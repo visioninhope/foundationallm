9839c7861636586b1a7a9295171aeef8a807db97
joelhulen <joelhulen@gmail.com>
2024-06-24 19:48:35 -0400

    Add attachment client

---
+++ src/dotnet/ManagementClient/Clients/Resources/AttachmentManagementClient.cs
@@ -0,0 +1,47 @@
+﻿using FoundationaLLM.Client.Management.Interfaces;
+using FoundationaLLM.Common.Constants.ResourceProviders;
+using FoundationaLLM.Common.Models.ResourceProviders;
+using FoundationaLLM.Common.Models.ResourceProviders.Attachment;
+
+namespace FoundationaLLM.Client.Management.Clients.Resources
+{
+    internal class AttachmentManagementClient(IManagementRESTClient managementRestClient) : IAttachmentManagementClient
+    {
+        /// <inheritdoc/>
+        public async Task<List<AttachmentFile>> GetAttachmentsAsync() => await managementRestClient.Resources.GetResourcesAsync<List<AttachmentFile>>(
+                ResourceProviderNames.FoundationaLLM_Attachment,
+                AttachmentResourceTypeNames.Attachments
+            );
+
+        /// <inheritdoc/>
+        public async Task<AttachmentFile> GetAttachmentAsync(string attachmentName)
+        {
+            var result = await managementRestClient.Resources.GetResourcesAsync<List<AttachmentFile>>(
+                ResourceProviderNames.FoundationaLLM_Attachment,
+                $"{AttachmentResourceTypeNames.Attachments}/{attachmentName}"
+            );
+
+            if (result == null || result.Count == 0)
+            {
+                throw new Exception($"Attachment '{attachmentName}' not found.");
+            }
+
+            var agent = result[0];
+
+            return agent;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ResourceProviderUpsertResult> UpsertAttachmentAsync(AttachmentFile attachment) => await managementRestClient.Resources.UpsertResourceAsync(
+            ResourceProviderNames.FoundationaLLM_Attachment,
+            $"{AttachmentResourceTypeNames.Attachments}/{attachment.Name}",
+            attachment
+            );
+
+        /// <inheritdoc/>
+        public async Task DeleteAttachmentAsync(string attachmentName) => await managementRestClient.Resources.DeleteResourceAsync(
+                ResourceProviderNames.FoundationaLLM_Attachment,
+                $"{AttachmentResourceTypeNames.Attachments}/{attachmentName}"
+            );
+    }
+}
