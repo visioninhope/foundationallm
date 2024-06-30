57d55c3404209788b1bcae057dcfc3e4efbec00a
Austin Hulen <austin@thehulens.com>
2024-06-29 21:25:37 -0700

    created branding page

---
+++ src/ui/ManagementPortal/pages/branding.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2 class="page-header">Branding</h2>
+        <div class="page-subheader">
+            <p>Customize the look and feel of your UI.</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import api from '@/js/api';
+
+export default {
+    name: 'Branding',
+
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss">
+
+</style>
