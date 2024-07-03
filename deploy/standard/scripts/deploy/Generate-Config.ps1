9c954f0f59451a749b4970e3ab4b251e94d054d7
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2024-07-03 12:32:33 -0500

    Adding logic to Generate-Config.ps1 to fetch OpenAI metadata in the BYO OpenAI case

--- deploy/standard/scripts/deploy/Generate-Config.ps1
+++ deploy/standard/scripts/deploy/Generate-Config.ps1
@@ -253,18 +253,38 @@ $vectorizationConfig = Invoke-AndRequireSuccess "Get Vectorization Config" {
 $tokens.vectorizationConfig = $vectorizationConfig
 
 $openAiEndpointUri = Invoke-AndRequireSuccess "Get OpenAI endpoint" {
-    az cognitiveservices account list `
-        --output tsv `
-        --query "[?contains(kind,'OpenAI')] | [0].properties.endpoint" `
-        --resource-group $($resourceGroups.oai) 
+    $oaiEndpoint=""
+    if ($manifest.deployOpenAi)
+    {
+        $oaiEndpoint = $(az cognitiveservices account list `
+            --output tsv `
+            --query "[?contains(kind,'OpenAI')] | [0].properties.endpoint" `
+            --resource-group $($resourceGroups.oai) )
+    } else {
+        $oaiEndpoint = $(az cognitiveservices account list `
+            --output tsv `
+            --query "[?contains(kind,'OpenAI') && name=='$($manifest.existingOpenAiInstance.name)'] | [0].properties.endpoint" `
+            --resource-group $($manifest.existingOpenAiInstance.resourceGroup) )
+    }
+    return $oaiEndpoint
 }
 $tokens.openAiEndpointUri = $openAiEndpointUri
 
 $openAiAccountName = Invoke-AndRequireSuccess "Get OpenAI Account name" {
-    az cognitiveservices account list `
-        --output tsv `
-        --query "[?contains(kind,'OpenAI')] | [0].id" `
-        --resource-group $($resourceGroups.oai) 
+    $oaiAccount = ""
+    if ($manifest.deployOpenAi)
+    {
+        $oaiAccount = $(az cognitiveservices account list `
+            --output tsv `
+            --query "[?contains(kind,'OpenAI')] | [0].id" `
+            --resource-group $($resourceGroups.oai) )
+    } else {
+        $oaiAccount = $(az cognitiveservices account list `
+            --output tsv `
+            --query "[?contains(kind,'OpenAI') && name=='$($manifest.existingOpenAiInstance.name)'] | [0].id" `
+            --resource-group $($manifest.existingOpenAiInstance.resourceGroup) )
+    }
+    return $oaiAccount
 }
 $tokens.azureOpenAiAccountName = $openAiAccountName
 
