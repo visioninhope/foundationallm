e664c7b74252f82f2607540db03958cfc061e010
kylebunting <kyle.bunting@gmail.com>
2024-07-17 15:36:29 -0600

    Adding async invoke method to LangChain KM agent

--- src/python/PythonSDK/foundationallm/langchain/agents/langchain_knowledge_management_agent.py
+++ src/python/PythonSDK/foundationallm/langchain/agents/langchain_knowledge_management_agent.py
@@ -8,7 +8,7 @@ from foundationallm.langchain.retrievers import RetrieverFactory, CitationRetrie
 from foundationallm.models.orchestration import (
     CompletionResponse
 )
-from foundationallm.models.agents import KnowledgeManagementCompletionRequest
+from foundationallm.models.agents import KnowledgeManagementAgent, KnowledgeManagementCompletionRequest
 from foundationallm.models.resource_providers.vectorization import (
     AzureAISearchIndexingProfile,
     AzureOpenAIEmbeddingProfile
@@ -97,10 +97,76 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
             raise e
 
         return response['label']
-    
-    async def invoke(self, request: KnowledgeManagementCompletionRequest) -> CompletionResponse:
+
+    def _get_document_retriever(agent: KnowledgeManagementAgent):
         """
-        Executes a completion request by querying the vector index with the user prompt.
+        Get the vector document retriever, if it exists.
+        """
+        retriever = None
+        if agent.vectorization is not None and not agent.inline_context:
+            text_embedding_profile = AzureOpenAIEmbeddingProfile.from_object(
+                agent.orchestration_settings.agent_parameters[
+                    agent.vectorization.text_embedding_profile_object_id])
+
+            indexing_profiles = []
+
+            if (agent.vectorization.indexing_profile_object_ids is not None) and (text_embedding_profile is not None):
+                for profile_id in agent.vectorization.indexing_profile_object_ids:
+                    indexing_profiles.append(
+                        AzureAISearchIndexingProfile.from_object(
+                            agent.orchestration_settings.agent_parameters[profile_id]
+                        )
+                    )
+
+                retriever_factory = RetrieverFactory(
+                                indexing_profiles,
+                                text_embedding_profile,
+                                self.config,
+                                request.settings)
+                retriever = retriever_factory.get_retriever()
+        return retriever
+
+    def _get_prompt_template(agent: KnowledgeManagementAgent, retriever: BaseRetriever = None) -> PromptTemplate:
+        """
+        Build a prompt template.
+        """
+        prompt = self._get_prompt_from_object_id(agent.prompt_object_id, agent.orchestration_settings.agent_parameters)
+
+        prompt_builder = ''
+
+        # Add the prefix, if it exists.
+        if prompt.prefix is not None:
+            prompt_builder = f'{prompt.prefix}\n\n'
+
+        # Add the message history, if it exists.
+        conversation_history = agent.conversation_history
+        if conversation_history is not None and conversation_history.enabled:
+            prompt_builder += self._build_conversation_history(
+                request.message_history,
+                conversation_history.max_history)
+
+        classification_prediction = self.get_prediction_from_audio_file(request.attachments)
+        if classification_prediction is not None:
+            prompt_builder += f"\n\CONTEXT: {classification_prediction}"
+        else:
+            # Insert the context into the template.
+            prompt_builder += '{context}'
+
+        # Add the suffix, if it exists.
+        if prompt.suffix is not None:
+            prompt_builder += f'\n\n{prompt.suffix}'
+ 
+        if retriever is not None:
+            # Insert the user prompt into the template.
+            prompt_builder += "\n\nQuestion: {question}"
+
+        # Create the prompt template.
+        return PromptTemplate.from_template(prompt_builder)
+
+    def invoke(self, request: KnowledgeManagementAgent) -> CompletionResponse:
+        """
+        Executes a synchronous completion request.
+        If a vector index exists, it will be queryied with the user prompt.
 
         Parameters
         ----------
@@ -114,68 +180,74 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
             generated full prompt with context and token utilization and execution cost details.
         """
         self._validate_request(request)
+        
         agent = request.agent
-
-        prompt = self._get_prompt_from_object_id(agent.prompt_object_id, agent.orchestration_settings.agent_parameters)
-
+        
         with get_openai_callback() as cb:
             try:
-                prompt_builder = ''
-
-                # Add the prefix, if it exists.
-                if prompt.prefix is not None:
-                    prompt_builder = f'{prompt.prefix}\n\n'
-
-                # Add the message history, if it exists.
-                conversation_history = agent.conversation_history
-                if conversation_history is not None and conversation_history.enabled:
-                    prompt_builder += self._build_conversation_history(
-                        request.message_history,
-                        conversation_history.max_history)
-
-                classification_prediction = self.get_prediction_from_audio_file(request.attachments)
-                if classification_prediction is not None:
-                    prompt_builder += f"\n\CONTEXT: {classification_prediction}"
-                else:
-                    # Insert the context into the template.
-                    prompt_builder += '{context}'
-
-                # Add the suffix, if it exists.
-                if prompt.suffix is not None:
-                    prompt_builder += f'\n\n{prompt.suffix}'
-
                 # Get the vector document retriever, if it exists.
-                retriever = None
-                if request.agent.vectorization is not None and not request.agent.inline_context:
+                retriever = self._get_document_retriever(agent)
+                # Get the prompt template.
+                prompt_template = self._get_prompt_template(agent, retriever)
 
-                    text_embedding_profile = AzureOpenAIEmbeddingProfile.from_object(
-                        agent.orchestration_settings.agent_parameters[
-                            agent.vectorization.text_embedding_profile_object_id])
-
-                    indexing_profiles = []
-
-                    if (agent.vectorization.indexing_profile_object_ids is not None) and (text_embedding_profile is not None):
-
-                        for profile_id in agent.vectorization.indexing_profile_object_ids:
-                            indexing_profiles.append(
-                                AzureAISearchIndexingProfile.from_object(
-                                    agent.orchestration_settings.agent_parameters[profile_id]
-                                )
-                            )
-
-                        retriever_factory = RetrieverFactory(
-                                        indexing_profiles,
-                                        text_embedding_profile,
-                                        self.config,
-                                        request.settings)
-                        retriever = retriever_factory.get_retriever()
-
-                # Insert the user prompt into the template.
                 if retriever is not None:
-                    prompt_builder += "\n\nQuestion: {question}"
+                    chain_context = { "context": retriever | retriever.format_docs, "question": RunnablePassthrough() }
+                else:
+                    chain_context = { "context": RunnablePassthrough() }
 
-                # Create the prompt template.
-                prompt_template = PromptTemplate.from_template(prompt_builder)
+                # Compose LCEL chain
+                chain = (
+                    chain_context
+                    | prompt_template
+                    | RunnableLambda(self._record_full_prompt)
+                    | self._get_language_model(agent.orchestration_settings, request.settings)
+                    | StrOutputParser()
+                )
+                
+                completion = chain.invoke(request.user_prompt)
+                citations = []
+                if isinstance(retriever, CitationRetrievalBase):
+                    citations = retriever.get_document_citations()
+
+                return CompletionResponse(
+                    completion = completion,
+                    citations = citations,
+                    user_prompt = request.user_prompt,
+                    full_prompt = self.full_prompt.text,
+                    completion_tokens = cb.completion_tokens,
+                    prompt_tokens = cb.prompt_tokens,
+                    total_tokens = cb.total_tokens,
+                    total_cost = cb.total_cost
+                )
+            except Exception as e:
+                raise LangChainException(f"An unexpected exception occurred when executing the completion request: {str(e)}", 500)
+
+    async def ainvoke(self, request: KnowledgeManagementCompletionRequest) -> CompletionResponse:
+        """
+        Executes an async completion request.
+        If a vector index exists, it will be queryied with the user prompt.
+
+        Parameters
+        ----------
+        request : KnowledgeManagementCompletionRequest
+            The completion request to execute.
+
+        Returns
+        -------
+        CompletionResponse
+            Returns a CompletionResponse with the generated summary, the user_prompt,
+            generated full prompt with context and token utilization and execution cost details.
+        """
+        self._validate_request(request)
+        
+        agent = request.agent
+        
+        with get_openai_callback() as cb:
+            try:
+                # Get the vector document retriever, if it exists.
+                retriever = self._get_document_retriever(agent)
+                # Get the prompt template.
+                prompt_template = self._get_prompt_template(agent, retriever)
 
                 if retriever is not None:
                     chain_context = { "context": retriever | retriever.format_docs, "question": RunnablePassthrough() }
