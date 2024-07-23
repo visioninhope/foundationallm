82d1d2256d4622c21310e2821c75679cb5a6e918
kylebunting <kyle.bunting@gmail.com>
2024-07-23 15:40:47 -0700

    Adding default value for last_updated field

--- src/python/PythonSDK/foundationallm/models/operations/long_running_operation.py
+++ src/python/PythonSDK/foundationallm/models/operations/long_running_operation.py
@@ -8,5 +8,5 @@ class LongRunningOperation(BaseModel):
     operation_id: str = Field(description='The unique identifier for the operation.')
     status: str = Field(description='The status of the operation.')
     status_message: str = Field(description='The message associated with the operation status.')
-    last_updated: datetime = Field(description='The timestamp of the last update to the operation.')
+    last_updated: datetime = Field(default=datetime.now(), description='The timestamp of the last update to the operation.')
     
