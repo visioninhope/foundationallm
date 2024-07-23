da3512d3230a9b5152141faaacf6d4a7ab5aa40d
kylebunting <kyle.bunting@gmail.com>
2024-07-22 17:17:50 -0700

    Removing unused objects

--- src/python/PythonSDK/foundationallm/models/orchestration/background_operation.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel, Field
-
-class BackgroundOperation(BaseModel):
-    operation_id: str = Field(description='The unique identifier for the operation.')
--- src/python/PythonSDK/foundationallm/state/state_manager.py
+++
@@ -1,7 +0,0 @@
-
-class StateManager():
-    def __init__():
-        pass
-
-    def get(self):
-        pass
