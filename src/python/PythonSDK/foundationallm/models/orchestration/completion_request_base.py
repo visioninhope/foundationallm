ba06bdae4055164bcb59fc62ce946f2a643e68b6
kylebunting <kyle.bunting@gmail.com>
2024-07-24 16:36:53 -0700

    Refactoring

--- src/python/PythonSDK/foundationallm/models/orchestration/completion_request_base.py
+++ src/python/PythonSDK/foundationallm/models/orchestration/completion_request_base.py
@@ -1,15 +1,13 @@
-import uuid
 from typing import List, Optional
 from pydantic import BaseModel
 from foundationallm.models.orchestration import MessageHistoryItem, OrchestrationSettings
 
 class CompletionRequestBase(BaseModel):
     """
-    Orchestration completion request.
+    Base class for completion requests.
     """
     operation_id: str
     session_id: Optional[str] = None
     user_prompt: str
-    attachments: Optional[List[str]] = None
     message_history: Optional[List[MessageHistoryItem]] = []
-    settings: Optional[OrchestrationSettings] = None
+    attachments: Optional[List[str]] = None
--- src/python/PythonSDK/foundationallm/models/orchestration/orchestration_settings.py
+++ src/python/PythonSDK/foundationallm/models/orchestration/orchestration_settings.py
@@ -1,23 +1,9 @@
-from typing import Optional
+from typing import Field, Optional
 from pydantic import BaseModel
 
 class OrchestrationSettings(BaseModel):
     """
-        Represents settings for the orchestration.
-        
-        Parameters
-        ----------
-        agent_parameters : dict
-            Parameters to set or override the behavior of the agent.
-        endpoint_configuration : dict
-            Parameters to set or override the endpoint configuration used by the orchestrator
-            to access a language model.
-        model_parameters : dict
-            Parameters to set or override the behavior of the language model as defined on the agent.
-        orchestrator : str
-            The orchestrator to use for the orchestration. Defaults to 'LangChain'.
+    Represents settings for the orchestration.
     """
-    agent_parameters: Optional[dict] = None
-    endpoint_configuration: Optional[dict] = None
-    model_parameters: Optional[dict] = None
-    orchestrator: Optional[str] = 'LangChain'
+    orchestrator: Optional[str] = Field(default='LangChain', description="The agent's LLM orchestrator type.")
+    agent_parameters: Optional[dict] = Field(default=None, description="A dictionary containing override values for the agent parameters.")
