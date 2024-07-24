e67fbbdb8f55c3dcde271b4a4db720efbf1438b2
kylebunting <kyle.bunting@gmail.com>
2024-07-24 16:33:56 -0700

    Removing unnecessary parameters and import references

--- src/python/PythonSDK/foundationallm/langchain/agents/agent_factory.py
+++ src/python/PythonSDK/foundationallm/langchain/agents/agent_factory.py
@@ -1,5 +1,4 @@
-from foundationallm.config import Configuration, Context
-from foundationallm.models.orchestration import CompletionRequestBase
+from foundationallm.config import Configuration
 from foundationallm.langchain.agents import (
     LangChainAgentBase,
     LangChainKnowledgeManagementAgent
@@ -10,44 +9,36 @@ class AgentFactory:
     Factory to determine which agent to use.
     """
 
-    def __init__(
-            self,
-            completion_request: CompletionRequestBase,
-            config: Configuration,
-            context: Context
-        ):
+    def __init__(self, config: Configuration):
         """
         Initializes an Orchestration for selecting which agent to use for completion.
 
         Parameters
         ----------
-        completion_request : CompletionRequest
-            The completion request object containing the user prompt to execute, message history,
-            and agent and data source metadata.
         config : Configuration
             Application configuration class for retrieving configuration settings.
-        context : Context
-            The user context under which to execution completion requests.    
         """
-        self.completion_request = completion_request
         self.config = config
-        self.context = context
 
-    def get_agent(self) -> LangChainAgentBase:
+    def get_agent(self, agent_type: str) -> LangChainAgentBase:
         """
         Retrieves an agent of the the requested type.
+
+        Parameters
+        ----------
+        agent_type : str
+            The type type assign to the agent returned.
         
         Returns
         -------
         AgentBase
             Returns an agent of the requested type.
         """
-        agent = self.completion_request.agent
-        if agent is None:
+        if agent_type is None:
             raise ValueError("Agent not constructed. Cannot access an object of 'NoneType'.")
         
-        match agent.type:
+        match agent_type:
             case 'knowledge-management':
                 return LangChainKnowledgeManagementAgent(config=self.config)
             case _:
-                raise ValueError(f'The agent type {agent.type} is not supported.')
+                raise ValueError(f'The agent type {agent_type} is not supported.')
