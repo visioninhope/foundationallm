1cac03647f65219487617b3e8d17e3e34dc19b22
kylebunting <kyle.bunting@gmail.com>
2024-07-24 16:36:27 -0700

    Refactoring to add objects array

--- src/python/PythonSDK/foundationallm/models/agents/knowledge_management_completion_request.py
+++ src/python/PythonSDK/foundationallm/models/agents/knowledge_management_completion_request.py
@@ -2,12 +2,13 @@
 Class Name: KnowledgeManagementCompletionRequest
 Description: Encapsulates the metadata required to complete a knowledge management orchestration request.
 """
-from typing import Optional
+from typing import List, Optional
 from .knowledge_management_agent import KnowledgeManagementAgent
 from foundationallm.models.orchestration import CompletionRequestBase
 
 class KnowledgeManagementCompletionRequest(CompletionRequestBase):
     """
-    Orchestration completion request.
+    The completion request received from the Orchestration API.
     """    
     agent: Optional[KnowledgeManagementAgent] = None
+    objects: List[dict] = []
