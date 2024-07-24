cd5121f7133c63150cd2595598e8c6c03b630588
kylebunting <kyle.bunting@gmail.com>
2024-07-24 16:35:29 -0700

    Refactoring

--- src/python/PythonSDK/foundationallm/models/agents/agent_base.py
+++ src/python/PythonSDK/foundationallm/models/agents/agent_base.py
@@ -1,19 +1,20 @@
 """
 Base Agent fulfilling the orchestration request.
 """
-from pydantic import BaseModel
+from pydantic import Field
 from typing import Optional
-from foundationallm.models.orchestration import OrchestrationSettings
-from foundationallm.models.agents import AgentConversationHistorySettings, AgentGatekeeperSettings
+from foundationallm.models.agents import (
+    AgentConversationHistorySettings,
+    AgentGatekeeperSettings,
+    AgentOrchestrationSettings
+)
+from foundationallm.models.resource_providers import ResourceBase
 
-class AgentBase(BaseModel):
+class AgentBase(ResourceBase):
     """ Agent Base model."""
-    name: str
-    description: Optional[str]
-    type: str
-    object_id: Optional[str] = None
-    prompt_object_id: Optional[str] = None
-    sessions_enabled: Optional[bool] = False
-    conversation_history: Optional[AgentConversationHistorySettings] = AgentConversationHistorySettings()
-    gatekeeper: Optional[AgentGatekeeperSettings] = AgentGatekeeperSettings()
-    orchestration_settings: Optional[OrchestrationSettings] = OrchestrationSettings()
+    sessions_enabled: bool = Field(default=False, description="Indicates whether sessions are enabled for the agent.")
+    conversation_history_settings: Optional[AgentConversationHistorySettings] = Field(default=AgentConversationHistorySettings(), description="Configuration for the agent's conversation history.")
+    gatekeeper_settings: Optional[AgentGatekeeperSettings] = Field(default=AgentGatekeeperSettings(), description="Gatekeeper configuration for the agent.")
+    orchestration_settings: Optional[AgentOrchestrationSettings] = Field(default=OrchestrationSettings(), description="Settings for the orchestrator.")
+    prompt_object_id: Optional[str] = Field(default=None, description="The object identifier of the Prompt object providing the prompt for the agent.")
+    ai_model_object_id: Optional[str] = Field(default=None, description="The object identifier of the AIModelBase object providing the AI model for the agent.")
