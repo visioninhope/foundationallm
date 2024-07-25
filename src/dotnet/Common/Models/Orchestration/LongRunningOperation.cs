8455979a01ec093bf927770da95d69f749e48417
joelhulen <joelhulen@gmail.com>
2024-07-25 16:21:11 -0400

    Add type and ID fields

--- src/dotnet/Common/Models/Orchestration/LongRunningOperation.cs
+++ src/dotnet/Common/Models/Orchestration/LongRunningOperation.cs
@@ -10,6 +10,18 @@ namespace FoundationaLLM.Common.Models.Orchestration
         /// <summary>
         /// The identifier of the long-running operation.
         /// </summary>
+        [JsonPropertyName("id")]
+        public string Id => OperationId;
+
+        /// <summary>
+        /// The document type.
+        /// </summary>
+        [JsonPropertyName("type")]
+        public string Type => nameof(LongRunningOperation);
+
+        /// <summary>
+        /// The identifier of the long-running operation.
+        /// </summary>
         [JsonPropertyName("operation_id")]
         public required string OperationId { get; set; }
 
