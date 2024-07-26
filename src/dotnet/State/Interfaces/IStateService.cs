61e3fca7f4432727f287f53f83c6cf1902571041
joelhulen <joelhulen@gmail.com>
2024-07-25 20:08:48 -0400

    Complete the State service methods

--- src/dotnet/State/Interfaces/IStateService.cs
+++ src/dotnet/State/Interfaces/IStateService.cs
@@ -1,9 +1,52 @@
-﻿namespace FoundationaLLM.State.Interfaces
+﻿using FoundationaLLM.Common.Models.Orchestration;
+
+namespace FoundationaLLM.State.Interfaces
 {
     /// <summary>
-    /// Defines the capabilities of the FoundationaLLM state service.
+    /// Provides methods for managing state for long-running operations.
     /// </summary>
     public interface IStateService
     {
+        /// <summary>
+        /// Retrieves all long-running operations.
+        /// </summary>
+        /// <returns></returns>
+        Task<List<LongRunningOperation>> GetLongRunningOperationsAsync();
+
+        /// <summary>
+        /// Retrieves a long-running operation by its identifier.
+        /// </summary>
+        /// <param name="id">The long-running operation identifier.</param>
+        /// <returns></returns>
+        Task<LongRunningOperation> GetLongRunningOperationAsync(string id);
+
+        /// <summary>
+        /// Retrieves all log entries for a long-running operation.
+        /// </summary>
+        /// <param name="operationId">The long-running operation identifier.</param>
+        /// <returns></returns>
+        Task<List<LongRunningOperationLogEntry>> GetLongRunningOperationLogEntriesAsync(string operationId);
+
+        /// <summary>
+        /// Retrieves the result of a long-running operation.
+        /// </summary>
+        /// <typeparam name="T">Define the type used to deserialize the operation result object.</typeparam>
+        /// <param name="operationId">The long-running operation identifier.</param>
+        /// <returns></returns>
+        Task<object?> GetLongRunningOperationResultAsync(string operationId);
+
+        /// <summary>
+        /// Inserts or updates a long-running operation and creates a log entry.
+        /// </summary>
+        /// <param name="operation">The long-running operation to insert or update.</param>
+        /// <returns></returns>
+        Task<bool> UpsertLongRunningOperationAsync(LongRunningOperation operation);
+
+        /// <summary>
+        /// Inserts or updates the result of a long-running operation.
+        /// </summary>
+        /// <param name="operationResult">The operation result to insert or update.</param>
+        /// <returns></returns>
+        Task<bool> UpsertLongRunningOperationResultAsync(dynamic operationResult);
     }
 }
--- src/dotnet/State/Services/StateService.cs
+++ src/dotnet/State/Services/StateService.cs
@@ -1,4 +1,5 @@
-﻿using FoundationaLLM.State.Interfaces;
+﻿using FoundationaLLM.Common.Models.Orchestration;
+using FoundationaLLM.State.Interfaces;
 using FoundationaLLM.State.Models.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -6,15 +7,56 @@ using Microsoft.Extensions.Options;
 namespace FoundationaLLM.State.Services
 {
     /// <summary>
-    /// Implements the FoundationaLLM state service.
+    /// Provides methods for managing state for long-running operations.
     /// </summary>
     /// <param name="options">Provides the options with the <see cref="StateServiceSettings"/> settings for configuration.</param>
+    /// <param name="cosmosDbService">Provides methods to interact with Cosmos DB.</param>
     /// <param name="logger">The <see cref="ILogger"/> used for logging.</param>
     public class StateService(
         IOptions<StateServiceSettings> options,
+        ICosmosDbService cosmosDbService,
         ILogger<StateService> logger) : IStateService
     {
-        private readonly StateServiceSettings _settings = options.Value;
-        private readonly ILogger<StateService> _logger = logger;
+        /// <inheritdoc/>
+        public async Task<List<LongRunningOperation>> GetLongRunningOperationsAsync()
+        {
+            logger.LogInformation("Getting long running operations.");
+            return await cosmosDbService.GetLongRunningOperationsAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task<LongRunningOperation> GetLongRunningOperationAsync(string id)
+        {
+            logger.LogInformation("Getting long running operation with ID: {id}", id);
+            return await cosmosDbService.GetLongRunningOperationAsync(id);
+        }
+
+        /// <inheritdoc/>
+        public async Task<List<LongRunningOperationLogEntry>> GetLongRunningOperationLogEntriesAsync(string operationId)
+        {
+            logger.LogInformation("Getting long running operation log entries for operation ID: {operationId}", operationId);
+            return await cosmosDbService.GetLongRunningOperationLogEntriesAsync(operationId);
+        }
+
+        /// <inheritdoc/>
+        public async Task<object?> GetLongRunningOperationResultAsync(string operationId)
+        {
+            logger.LogInformation("Getting long running operation result for operation ID: {operationId}", operationId);
+            return await cosmosDbService.GetLongRunningOperationResultAsync(operationId);
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> UpsertLongRunningOperationAsync(LongRunningOperation operation)
+        {
+            logger.LogInformation("Upserting long running operation.");
+            return await cosmosDbService.UpsertLongRunningOperationAsync(operation);
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> UpsertLongRunningOperationResultAsync(dynamic operationResult)
+        {
+            logger.LogInformation("Upserting long running operation result.");
+            return await cosmosDbService.UpsertLongRunningOperationResultAsync(operationResult);
+        }
     }
 }
