7599cbc3542eee897e56cb6a9c8afd0b17ed15c6
joelhulen <joelhulen@gmail.com>
2024-07-27 21:08:27 -0400

    Configure the GatewayAdapter API to use FLLM Instance paths

--- src/dotnet/GatewayAdapterAPI/Program.cs
+++ src/dotnet/GatewayAdapterAPI/Program.cs
@@ -4,7 +4,10 @@ using FoundationaLLM.Common.Authentication;
 using FoundationaLLM.Common.Constants;
 using FoundationaLLM.Common.Constants.Configuration;
 using FoundationaLLM.Common.Interfaces;
+using FoundationaLLM.Common.Middleware;
+using FoundationaLLM.Common.Models.Context;
 using FoundationaLLM.Common.OpenAPI;
+using FoundationaLLM.Common.Services.Security;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -24,6 +27,7 @@ builder.Configuration.AddAzureAppConfiguration(options =>
     {
         options.SetCredential(DefaultAuthentication.AzureCredential);
     });
+    options.Select(AppConfigurationKeyFilters.FoundationaLLM_Instance);
     options.Select(AppConfigurationKeyFilters.FoundationaLLM_APIs_GatewayAdapterAPI);
     options.Select(AppConfigurationKeyFilters.FoundationaLLM_Gateway);
 });
@@ -33,6 +37,8 @@ if (builder.Environment.IsDevelopment())
 builder.AddOpenTelemetry(
     AppConfigurationKeys.FoundationaLLM_APIs_GatewayAdapterAPI_AppInsightsConnectionString,
     ServiceNames.GatewayAdapterAPI);
+
+builder.Services.AddInstanceProperties(builder.Configuration);
 
 // CORS policies
 builder.AddCorsPolicies();
@@ -50,9 +56,18 @@ builder.Services.AddControllers();
 
 // Add API Key Authorization
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ICallContext, CallContext>();
+builder.Services.AddScoped<IUserClaimsProviderService, NoOpUserClaimsProviderService>();
 builder.Services.AddScoped<APIKeyAuthenticationFilter>();
 builder.Services.AddOptions<APIKeyValidationSettings>()
     .Bind(builder.Configuration.GetSection(AppConfigurationKeySections.FoundationaLLM_APIs_GatewayAdapterAPI));
+builder.Services.AddTransient<IAPIKeyValidationService, APIKeyValidationService>();
+
+// Add authorization services.
+builder.AddGroupMembership();
+
+// Add services to the container.
+builder.Services.AddAuthorization();
 
 builder.Services
     .AddApiVersioning(options =>
@@ -89,7 +104,12 @@ var app = builder.Build();
 // Set the CORS policy before other middleware.
 app.UseCors(CorsPolicyNames.AllowAllOrigins);
 
-app.UseExceptionHandler();
+// Register the middleware to extract the user identity context and other HTTP request context data required by the downstream services.
+app.UseMiddleware<CallContextMiddleware>();
+
+app.UseExceptionHandler(exceptionHandlerApp
+    => exceptionHandlerApp.Run(async context
+        => await Results.Problem().ExecuteAsync(context)));
 
 app.UseSwagger();
 app.UseSwaggerUI(
