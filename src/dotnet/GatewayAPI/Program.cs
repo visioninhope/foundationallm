5cbe9957a894f571e23dc3e0999bcf78908ed92f
joelhulen <joelhulen@gmail.com>
2024-07-29 21:06:13 -0400

    Remove old App Config filter

--- src/dotnet/GatewayAPI/Program.cs
+++ src/dotnet/GatewayAPI/Program.cs
@@ -29,7 +29,6 @@ builder.Configuration.AddAzureAppConfiguration(options =>
         options.SetCredential(DefaultAuthentication.AzureCredential);
     });
     options.Select(AppConfigurationKeyFilters.FoundationaLLM_Instance);
-    options.Select(AppConfigurationKeyFilters.FoundationaLLM_Gateway);
     options.Select(AppConfigurationKeyFilters.FoundationaLLM_APIEndpoints_GatewayAPI);
 });
 if (builder.Environment.IsDevelopment())
