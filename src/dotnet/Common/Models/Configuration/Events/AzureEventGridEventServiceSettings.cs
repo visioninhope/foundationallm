102d434c998e893a7ace4581e12ee8a969d904f3
Ciprian Jichici <ciprian@solliance.net>
2024-08-04 23:42:43 +0300

    Create Azure Event Grid client using APIEndpointConfiguration

--- src/dotnet/Common/Models/Configuration/Events/AzureEventGridEventServiceSettings.cs
+++ src/dotnet/Common/Models/Configuration/Events/AzureEventGridEventServiceSettings.cs
@@ -1,6 +1,4 @@
-﻿using FoundationaLLM.Common.Constants.Authentication;
-using FoundationaLLM.Common.Services.Events;
-using System.Text.Json.Serialization;
+﻿using FoundationaLLM.Common.Services.Events;
 
 namespace FoundationaLLM.Common.Models.Configuration.Events
 {
@@ -9,23 +7,6 @@ namespace FoundationaLLM.Common.Models.Configuration.Events
     /// </summary>
     public class AzureEventGridEventServiceSettings
     {
-        /// <summary>
-        /// A <see cref="AuthenticationTypes"/> value indicating the type of authentication used.
-        /// </summary>
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        public required AuthenticationTypes AuthenticationType { get; set; }
-
-        /// <summary>
-        /// The Azure Event Grid namespace endpoint.
-        /// </summary>
-        public string? Endpoint { get; set; }
-
-        /// <summary>
-        /// The API key used for authentication.
-        /// This value should be set only if AuthenticationType has a value of APIKey.
-        /// </summary>
-        public string? APIKey { get; set; }
-
         /// <summary>
         /// The Azure resource identifier for the Azure Event Grid namespace.
         /// </summary>
