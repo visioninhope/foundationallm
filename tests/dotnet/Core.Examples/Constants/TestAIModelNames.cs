133982b524afc0bb0b18b42f69485aa9a4624716
codingbandit <carey.payette@gmail.com>
2024-08-06 12:54:00 -0400

    Fix compilation of tests

--- tests/dotnet/Core.Examples/Constants/TestAIModelNames.cs
+++ tests/dotnet/Core.Examples/Constants/TestAIModelNames.cs
@@ -14,5 +14,15 @@ namespace FoundationaLLM.Core.Examples.Constants
         /// The name of the GPT-4-32K completions AI model.
         /// </summary>
         public const string Completions_GPT4_32K = "completions-gpt-4-32k";
+
+        /// <summary>
+        /// The name of the default completions AI model deployed in new FoundationaLLM environments.
+        /// </summary>
+        public const string Completions_Deployed_Default = "DefaultCompletionAIModel";
+        
+        /// <summary>
+        /// The name of the default embeddings AI model.
+        /// </summary>
+        public const string Embeddings_Default = "embeddings";
     }
 }
