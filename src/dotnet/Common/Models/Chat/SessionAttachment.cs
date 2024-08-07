d6d0270215686396a4b2ed8c1a1e81a2caa1bc4a
joelhulen <joelhulen@gmail.com>
2024-08-07 09:43:54 -0400

    Create session attachment object

---
+++ src/dotnet/Common/Models/Chat/SessionAttachment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Common.Models.Chat
+{
+    public class SessionAttachment
+    {
+        /// <summary>
+        /// The unique identifier of the attachment resource.
+        /// </summary>
+        [JsonPropertyName("object_id")]
+        public string? ObjectId { get; set; }
+
+        [JsonPropertyName("date_added")]
+        public DateTime DateAdded { get; set; }
+
+        /// <summary>
+        /// The attachment file name.
+        /// </summary>
+        [JsonPropertyName("display_name")]
+        public string? DisplayName { get; set; }
+
+        /// <summary>
+        /// The mime content type of the attachment.
+        /// </summary>
+        [JsonPropertyName("content_type")]
+        public string? ContentType { get; set; }
+    }
+}
