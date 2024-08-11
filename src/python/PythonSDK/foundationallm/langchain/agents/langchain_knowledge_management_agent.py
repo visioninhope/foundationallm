6cb54a7221d7681530b14534912b81ba93585810
kylebunting <kyle.bunting@gmail.com>
2024-08-11 12:35:05 -0600

    Adding agent capability categories to content items.

--- src/python/PythonSDK/foundationallm/langchain/agents/langchain_knowledge_management_agent.py
+++ src/python/PythonSDK/foundationallm/langchain/agents/langchain_knowledge_management_agent.py
@@ -5,6 +5,7 @@ from langchain_core.output_parsers import StrOutputParser
 from foundationallm.langchain.agents import LangChainAgentBase
 from foundationallm.langchain.exceptions import LangChainException
 from foundationallm.langchain.retrievers import RetrieverFactory, CitationRetrievalBase
+from foundationallm.models.constants import AgentCapabilityCategories
 from foundationallm.models.orchestration import (
     CompletionResponse
 )
@@ -87,7 +88,8 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
         if self.prompt.suffix is not None:
             prompt_builder += f'\n\n{self.prompt.suffix}'
 
-        if self.has_retriever or len(request.attachments) > 0:
+        image_attachments = [attachment for attachment in request.attachments if (attachment.provider == AttachmentProviders.FOUNDATIONALLM_ATTACHMENTS and attachment.content_type.startswith('image/'))] if request.attachments is not None else []
+        if self.has_retriever or len(image_attachments) > 0:
             # Insert the user prompt into the template.
             prompt_builder += "\n\nQuestion: {question}"
 
@@ -241,14 +243,14 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
                     thread_id = assistant_req.thread_id,
                     role = "user",
                     content = "Analyze any attached images.",
-                    attachments= []
+                    attachments = []
                 )
                 # Add assistant message
                 assistant_svc.add_thread_message(
                     thread_id = assistant_req.thread_id,
                     role = "assistant",
                     content = image_analysis_svc.format_results(image_analysis_results),
-                    attachments= []
+                    attachments = []
                 )
             # invoke/run the service
             assistant_response = assistant_svc.run(assistant_req)
@@ -294,7 +296,8 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
 
                 completion = chain.invoke(request.user_prompt)
                 response_content = OpenAITextMessageContentItem(
-                    value=completion
+                    value = completion,
+                    agent_capability_category = AgentCapabilityCategories.FOUNDATIONALLM_KNOWLEDGE_MANAGEMENT
                 )
                 
                 citations = []
@@ -364,14 +367,14 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
                     thread_id = assistant_req.thread_id,
                     role = "user",
                     content = "Analyze any attached images.",
-                    attachments= []
+                    attachments = []
                 )
                 # Add assistant message
                 await assistant_svc.aadd_thread_message(
                     thread_id = assistant_req.thread_id,
                     role = "assistant",
                     content = image_analysis_svc.format_results(image_analysis_results),
-                    attachments= []
+                    attachments = []
                 )
 
             # invoke/run the service
@@ -423,7 +426,8 @@ class LangChainKnowledgeManagementAgent(LangChainAgentBase):
                     completion = await chain.ainvoke(request.user_prompt)
 
                 response_content = OpenAITextMessageContentItem(
-                    value=completion
+                    value = completion,
+                    agent_capability_category = AgentCapabilityCategories.FOUNDATIONALLM_KNOWLEDGE_MANAGEMENT
                 )
 
                 citations = []
--- src/python/PythonSDK/foundationallm/models/orchestration/message_content_item_base.py
+++ src/python/PythonSDK/foundationallm/models/orchestration/message_content_item_base.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel, Field, Extra
 from typing import Optional
 from .message_content_item_types import MessageContentItemTypes
+from foundationallm.models.constants import AgentCapabilityCategories
 
 class MessageContentItemBase(BaseModel):
     """Base message content item model."""
     
-    type: Optional[MessageContentItemTypes] = Field(None, alias="type")
+    type: Optional[MessageContentItemTypes] = Field(None, description="The type of message content item.")
+    agent_capability_category: Optional[AgentCapabilityCategories] = Field(None, description="The category of capability assigned to the agent.")
     
     class Config:
         use_enum_values = True
