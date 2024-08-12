5f7a6bd780f05bca120dd7140fddf35d56375406
kylebunting <kyle.bunting@gmail.com>
2024-08-12 10:37:49 -0600

    Fixing attachment provider name

--- src/python/PythonSDK/foundationallm/models/attachments/attachment_providers.py
+++ src/python/PythonSDK/foundationallm/models/attachments/attachment_providers.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 class AttachmentProviders(str, Enum):
     """Enumerator of the Attachment Providers."""
-    FOUNDATIONALLM_ATTACHMENTS = "FoundationaLLM.Attachments"
+    FOUNDATIONALLM_ATTACHMENT = "FoundationaLLM.Attachment"
     FOUNDATIONALLM_AZURE_OPENAI = "FoundationaLLM.AzureOpenAI"
