abad23c1ce800088c7fa1e4aae71d50a3585179a
Ciprian Jichici <ciprian@solliance.net>
2024-08-13 03:26:18 +0300

    Fix issue with default configuration for Gateway API

---
+++ tests/dotnet/Core.Examples/Utils/TimeProfiler.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Xunit.Abstractions;
+
+namespace FoundationaLLM.Core.Examples.Utils
+{
+    public class TimeProfiler(
+        ITestOutputHelper output)
+    {
+        private readonly ITestOutputHelper _output = output;
+
+
+        public async Task RunAsync(Func<Task> action, string actionName)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            await action();
+
+            stopwatch.Stop();
+            _output.WriteLine($"{actionName} took {stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
