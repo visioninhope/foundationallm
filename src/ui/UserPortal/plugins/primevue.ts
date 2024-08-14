1e1d91e9db95d9a6d868886d8e47a0ac95812988
Austin Hulen <austin@thehulens.com>
2024-08-14 02:11:49 -0700

    Add autofocus and focus trap to chat rename modal

--- src/ui/UserPortal/plugins/primevue.ts
+++ src/ui/UserPortal/plugins/primevue.ts
@@ -18,6 +18,7 @@ import BadgeDirective from 'primevue/badgedirective';
 import ConfirmDialog from 'primevue/confirmdialog';
 import ConfirmationService from 'primevue/confirmationservice';
 import ProgressBar from 'primevue/progressbar';
+import FocusTrap from 'primevue/focustrap';
 
 import { defineNuxtPlugin } from '#app';
 
@@ -39,6 +40,7 @@ export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.directive('badge', BadgeDirective);
 	nuxtApp.vueApp.component('ConfirmDialog', ConfirmDialog);
 	nuxtApp.vueApp.component('ProgressBar', ProgressBar);
+	nuxtApp.vueApp.directive('focus-trap', FocusTrap);
 
 	nuxtApp.vueApp.use(ConfirmationService);
 
