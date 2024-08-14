30d1f68506db634a4cd0caf8f859b137189a4b86
kylebunting <kyle.bunting@gmail.com>
2024-08-14 07:43:30 -0600

    Updating attachment assignment to use OpenAI.Assistants route

--- src/dotnet/Orchestration/Orchestration/KnowledgeManagementOrchestration.cs
+++ src/dotnet/Orchestration/Orchestration/KnowledgeManagementOrchestration.cs
@@ -127,9 +127,8 @@ namespace FoundationaLLM.Orchestration.Core.Orchestration
             List<AttachmentProperties> result = [];
             await foreach (var attachment in attachments)
             {
-                var useAttachmentPath =
-                    string.IsNullOrWhiteSpace(attachment.SecondaryProvider)
-                    || (attachment.ContentType ?? string.Empty).StartsWith("image/", StringComparison.OrdinalIgnoreCase);
+                // Use the attachment path if a secondary provider is not set.
+                var useAttachmentPath = string.IsNullOrWhiteSpace(attachment.SecondaryProvider);
 
                 result.Add(new AttachmentProperties
                 {
