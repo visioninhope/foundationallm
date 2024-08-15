a87393fe1279dbd3ccf1acd57750775a25ed3fc5
joelhulen <joelhulen@gmail.com>
2024-08-15 01:07:57 -0400

    Improve attachment resource saving

--- src/ui/UserPortal/components/ChatMessage.vue
+++ src/ui/UserPortal/components/ChatMessage.vue
@@ -73,7 +73,7 @@
 										content.loading = false;
 										content.error = true;
 									"
-									width="75%"
+									width="45%"
 									preview
 								/>
 								<div v-if="content.error" class="loading-image-error">
@@ -407,6 +407,7 @@ export default {
 						const response = await api.fetchDirect(content.value);
 						const blobUrl = URL.createObjectURL(response);
 						content.blobUrl = blobUrl;
+						content.fileName = content.fileName?.split('/').pop();
 					} catch (error) {
 						console.error(`Failed to fetch content from ${content.value}`, error);
 						content.error = true;
