a996e6ff8cd3fc911ebc06671f016a43b4d362d2
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2024-08-19 20:28:47 +0300

    Improve conversation generation

--- tests/dotnet/Core.Examples/Constants/TestAIModelNames.cs
+++ tests/dotnet/Core.Examples/Constants/TestAIModelNames.cs
@@ -16,6 +16,11 @@
         public const string Completions_GPT4_32K = "completions-gpt-4-32k";
 
         /// <summary>
+        /// The name of the GPT-4o completions AI model.
+        /// </summary>
+        public const string Completions_GPT4o = "GPT4oCompletionAIModel";
+
+        /// <summary>
         /// The name of the default completions AI model deployed in new FoundationaLLM environments.
         /// </summary>
         public const string Completions_Deployed_Default = "DefaultCompletionAIModel";
