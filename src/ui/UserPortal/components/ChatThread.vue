72ddec1546e7d4c59a8ec87c2709e2b2a546a927
Austin Hulen <austin@thehulens.com>
2024-08-26 04:35:00 -0700

    Chat log content now follows logical order

--- src/ui/UserPortal/components/ChatThread.vue
+++ src/ui/UserPortal/components/ChatThread.vue
@@ -2,7 +2,8 @@
 	<div class="chat-thread">
 		<!-- Message list -->
 		<div class="chat-thread__messages"
-			:class="messages.length === 0 && 'empty'">
+			:class="messages.length === 0 && 'empty'"
+			ref="messageContainer">
 			<template v-if="isLoading">
 				<div class="chat-thread__loading">
 					<i class="pi pi-spin pi-spinner" style="font-size: 2rem"></i>
@@ -12,8 +13,9 @@
 			<template v-else>
 				<!-- Messages -->
 				<template v-if="messages.length !== 0">
+					<div class="spacer" />
 					<ChatMessage
-						v-for="(message, index) in messages.slice().reverse()"
+						v-for="(message, index) in messages.slice()"
 						:key="`${message.id}-${componentKey}`"
 						:message="message"
 						:show-word-animation="index === 0 && userSentMessage && message.sender === 'Assistant'"
@@ -90,6 +92,11 @@ export default {
 
 	mounted() {
 		eventBus.on('operation-completed', this.handleOperationCompleted);
+		this.scrollToBottom();
+	},
+
+	updated() {
+		this.scrollToBottom();
 	},
 
 	methods: {
@@ -158,6 +165,12 @@ export default {
 				await this.$appStore.getMessages();
 			}
 		},
+
+		scrollToBottom() {
+            this.$nextTick(() => {
+                this.$refs.messageContainer.scrollTop = this.$refs.messageContainer.scrollHeight;
+            });
+        },
 	},
 };
 </script>
@@ -189,7 +202,7 @@ export default {
 
 .chat-thread__messages {
 	display: flex;
-	flex-direction: column-reverse;
+	flex-direction: column;
 	flex: 1;
 	overflow-y: auto;
 	overscroll-behavior: auto;
@@ -241,5 +254,9 @@ footer {
 	text-align: right;
 	font-size: 0.85rem;
 	padding-right: 24px;
+}
+
+.spacer {
+	flex-grow: 1;
 }
 </style>
