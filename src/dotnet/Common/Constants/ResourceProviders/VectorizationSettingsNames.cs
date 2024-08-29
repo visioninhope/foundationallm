ec36483d5b4178fa04a450d27419d878e048e30e
Carey Payette <carey.payette@gmail.com>
2024-08-28 21:31:19 -0400

    Introduced constants for Vectorization profile Settings key names.

---
+++ src/dotnet/Common/Constants/ResourceProviders/VectorizationSettingsNames.cs
@@ -0,0 +1,19 @@
+﻿namespace FoundationaLLM.Common.Constants.ResourceProviders
+{
+    /// <summary>
+    /// Contains constants of the names of the settings contained within vectorization profiles.
+    /// </summary>
+    public class VectorizationSettingsNames
+    {
+        /// <summary>
+        /// The settings name/key of the embedding model for an embedding profile.
+        /// </summary>
+        public const string EmbeddingProfileModelName = "model_name";
+
+
+        /// <summary>
+        /// The settings/key name of the api endpoint configuration object id for an indexing profile.
+        /// </summary>
+        public const string IndexingProfileApiEndpointConfigurationObjectId = "api_endpoint_configuration_object_id";
+    }
+}
